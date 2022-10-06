209aae5d32ddefb533b08d09df110d7303c3b4f5
rayedsikder <rayedsikder33@gmail.com>
2022-10-06 20:01:20 +0600

    Add CampaignTreasury contract structure

---
+++ contracts/CampaignTreasury.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract CampaignTreasury {
+    address oracleAddress;
+    address infoAddress;
+    bytes32 clientId;
+    uint256 feePercent;
+
+    function getPledgeAmount() public view returns(uint256 pledgedAmount) {
+        
+    }
+
+    function getCollectableByCreator() public view returns(uint256 collectableAmount) {
+
+    }
+}
