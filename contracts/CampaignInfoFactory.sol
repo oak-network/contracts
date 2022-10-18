6fda2e104c33e199e98bc163d5423ce5a56c825a
rayedsikder <rayedsikder33@gmail.com>
2022-10-18 12:23:49 +0600

    Add setRegistry & checks for CampaignInfo creation

--- contracts/CampaignInfoFactory.sol
+++ contracts/CampaignInfoFactory.sol
@@ -4,14 +4,22 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./CampaignInfo.sol";
+import "./CampaignRegistry.sol";
 
 contract CampaignInfoFactory is Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter public campaignId;
     CampaignInfo newCampaignInfo;
+    address campaignRegistry;
+    bool initialized;
     
     mapping(uint256 => address) public campaignIdToAddress;
+
+    function setRegistry(address _campaignRegistry) public onlyOwner {
+        campaignRegistry = _campaignRegistry;
+        initialized = true;
+    }
 
     function createCampaign(
         uint256 creatorId, 
@@ -21,8 +29,14 @@ contract CampaignInfoFactory is Ownable {
         bytes32[] memory multilistClients
     ) external onlyOwner
     {
+        require(initialized);
         newCampaignInfo = new CampaignInfo(creatorId, goal, launchTime, deadline, multilistClients);
-        campaignIdToAddress[campaignId.current()] = address(newCampaignInfo);
+        require(address(newCampaignInfo) != address(0));
+        CampaignRegistry(campaignRegistry).setCampaignInfoAddress
+        (
+            campaignId.current(), 
+            address(newCampaignInfo)
+        );
         campaignId.increment();
     }
 
