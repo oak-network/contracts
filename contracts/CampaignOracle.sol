9136b577986db71e62de715ca7ac562757b54be1
rayedsikder <rayedsikder33@gmail.com>
2022-10-19 10:47:57 +0600

    Add initializers and implementation of registry

--- contracts/CampaignOracle.sol
+++ contracts/CampaignOracle.sol
@@ -2,15 +2,25 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./CampaignRegistry.sol";
 
 contract CampaignOracle is Ownable {
-    mapping(address => mapping (bytes32 => uint256)) pledgeAcrossClientsByCampaign;
+    //mapping(address => mapping (bytes32 => uint256)) pledgeAcrossClientsByCampaign;
+
+    address registryAddress;
+    
+    function initialize(address _registryAddress) public onlyOwner {
+        registryAddress = _registryAddress;
+    }
 
     function setPledgeAmountForClient(bytes32 clientId, address campaignAddress, uint256 pledgeAmount) onlyOwner external {
-        pledgeAcrossClientsByCampaign[campaignAddress][clientId] = pledgeAmount;
+//      pledgeAcrossClientsByCampaign[campaignAddress][clientId] = pledgeAmount;
+        CampaignTreasury(CampaignRegistry(registryAddress).getTreasuryAddress(campaignAddress, clientId)).setPledgeAmount(pledgeAmount);
+
     }
 
     function getPledgeAmountForClient(bytes32 clientId, address campaignAddress) public view returns(uint256) {
-        return pledgeAcrossClientsByCampaign[campaignAddress][clientId];
+        //return pledgeAcrossClientsByCampaign[campaignAddress][clientId];
+        return CampaignTreasury(CampaignRegistry(registryAddress).getTreasuryAddress(campaignAddress, clientId)).getPledgeAmount();
     }
 }
