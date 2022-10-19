9fac94c776558bfb42f67038fd45db9a7bf6ba02
rayedsikder <rayedsikder33@gmail.com>
2022-10-19 10:47:23 +0600

    Add functions and variables for setting pledge

--- contracts/CampaignTreasury.sol
+++ contracts/CampaignTreasury.sol
@@ -4,11 +4,12 @@ pragma solidity ^0.8.9;
 import "./CampaignOracle.sol";
 import "./CampaignInfo.sol";
 
-contract CampaignTreasury {
+contract CampaignTreasury is Ownable {
     address oracleAddress;
     address infoAddress;
-    bytes32 clientId;
+    bytes32 public clientId;
     uint256 feePercent;
+    uint256 pledgedAmount;
     uint256 public constant percentDivider = 10000;
 
     constructor(
@@ -23,12 +24,18 @@ contract CampaignTreasury {
         feePercent = _feePercent;
     }
 
-    function getPledgeAmount() public view returns(uint256 pledgedAmount) {
-        return CampaignOracle(oracleAddress).getPledgeAmountForClient(clientId, infoAddress);
+    function getPledgeAmount() public view returns(uint256) {
+        //return CampaignOracle(oracleAddress).getPledgeAmountForClient(clientId, infoAddress);
+        return pledgedAmount;
     }
 
+    // Old - not required by current API endpoints
     function getCollectableByCreator() public view returns(uint256 collectableAmount) {
         return CampaignOracle(oracleAddress).getPledgeAmountForClient(clientId, infoAddress) * 
         feePercent / percentDivider;
+    }
+
+    function setPledgeAmount(uint256 _pledgedAmount) public onlyOwner {
+        pledgedAmount = _pledgedAmount;
     }
 }
