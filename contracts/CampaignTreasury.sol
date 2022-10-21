3010924fe375c2c4ccdedc2b5a708fd9130160db
rayedsikder <rayedsikder33@gmail.com>
2022-10-21 12:56:35 +0600

    Fix issues with treasury parameters

--- contracts/CampaignTreasury.sol
+++ contracts/CampaignTreasury.sol
@@ -5,23 +5,20 @@ import "./CampaignOracle.sol";
 import "./CampaignInfo.sol";
 
 contract CampaignTreasury is Ownable {
-    address oracleAddress;
+    address registryAddress;
     address infoAddress;
     bytes32 public clientId;
-    uint256 feePercent;
     uint256 pledgedAmount;
     uint256 public constant percentDivider = 10000;
 
     constructor(
-        address _oracleAddress,
+        address _registryAddress,
         address _infoAddress,
         bytes32 _clientId,
-        uint256 _feePercent
     ) {
-        oracleAddress = _oracleAddress;
+        registryAddress = _registryAddress;
         infoAddress = _infoAddress;
         clientId = _clientId;
-        feePercent = _feePercent;
     }
 
     function getPledgeAmount() public view returns(uint256) {
@@ -30,10 +27,10 @@ contract CampaignTreasury is Ownable {
     }
 
     // Old - not required by current API endpoints
-    function getCollectableByCreator() public view returns(uint256 collectableAmount) {
-        return CampaignOracle(oracleAddress).getPledgeAmountForClient(clientId, infoAddress) * 
-        feePercent / percentDivider;
-    }
+    // function getCollectableByCreator() public view returns(uint256 collectableAmount) {
+    //     return CampaignOracle(oracleAddress).getPledgeAmountForClient(clientId, infoAddress) * 
+    //     feePercent / percentDivider;
+    // }
 
     function setPledgeAmount(uint256 _pledgedAmount) public onlyOwner {
         pledgedAmount = _pledgedAmount;
