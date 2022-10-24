863de8005ad6cef95ec57156cf3a0b583f0dd002
rayedsikder <rayedsikder33@gmail.com>
2022-10-24 15:27:23 +0600

    Remove dependencies on CampaignRegistry

--- contracts/CampaignOracle.sol
+++ contracts/CampaignOracle.sol
@@ -6,18 +6,10 @@ import "./CampaignRegistry.sol";
 import "./CampaignInfo.sol";
 
 contract CampaignOracle is Ownable {
-    //mapping(address => mapping (bytes32 => uint256)) pledgeAcrossClientsByCampaign;
-
-    address registryAddress;
-    
-    function initialize(address _registryAddress) public onlyOwner {
-        registryAddress = _registryAddress;
-    }
 
     function setPledgeAmountForClient(bytes32 clientId, address campaignAddress, uint256 pledgeAmount) onlyOwner external {
         require(CampaignInfo(campaignAddress).getTreasuryAddress(clientId) != address(0));
         CampaignTreasury(CampaignInfo(campaignAddress).getTreasuryAddress(clientId)).setPledgeAmount(pledgeAmount);
-
     }
 
     function getPledgeAmountForClient(bytes32 clientId, address campaignAddress) public view returns(uint256) {
