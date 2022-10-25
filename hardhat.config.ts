d344e2b5ebe5f07218a645ac84e12328a1821024
rayedsikder <rayedsikder33@gmail.com>
2022-10-25 14:03:38 +0600

    Setup network configurations

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const PRIVATE_KEY = process.env;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    alfajores: {
+      url: `https://alfajores-forno.celo-testnet.org`,
+      accounts: [PRIVATE_KEY]
+    }
+  }
 };
 
 export default config;
