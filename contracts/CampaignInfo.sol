a339bee1e86dcd2aedb26abedee0acbc9717fd5b
rayedsikder <rayedsikder33@gmail.com>
2022-10-26 09:41:18 +0600

    Fix issues with pledge total & refactor data types

--- contracts/CampaignInfo.sol
+++ contracts/CampaignInfo.sol
@@ -7,42 +7,42 @@ import "./CampaignRegistry.sol";
 
 contract CampaignInfo is Ownable {
 
-    struct Campaign {
+    struct CampaignData {
         string identifier;
         bytes32 originPlatform;
         uint64 goalAmount;
-        uint64 startsAt;
+        uint64 launchTime;
         uint64 createdAt;
         uint64 deadline;
         string creatorUrl;
         bytes32[] reachPlatforms;
     }
 
-    Campaign campaign;
+    CampaignData campaign;
     address registryAddress;
 
-    mapping(bytes32 => address) treasuryAddresses;
+    mapping (bytes32 => address) treasuryAddress;
     
     constructor(       
         string memory  _identifier,
         bytes32 _originPlatform,
         uint64 _goalAmount,
-        uint64 _startsAt,
+        uint64 _launchTime,
         uint64 _deadline,
         string memory _creatorUrl,
-        bytes32[] memory _reachPlatforms,
+        bytes32[] memory _reachPlatform,
         address _registryAddress
     )
     {
-        require(_startsAt + 30 days < _deadline);
+        require(_launchTime + 30 days < _deadline);
         campaign.identifier = _identifier;
         campaign.originPlatform = _originPlatform;
         campaign.goalAmount = _goalAmount;
-        campaign.startsAt = _startsAt;
+        campaign.launchTime = _launchTime;
         campaign.createdAt = uint64(block.timestamp);
         campaign.deadline = _deadline;
         campaign.creatorUrl = _creatorUrl;
-        campaign.reachPlatforms = _reachPlatforms;
+        campaign.reachPlatforms = _reachPlatform;
         registryAddress = _registryAddress;
     }
 
@@ -50,33 +50,68 @@ contract CampaignInfo is Ownable {
         require(msg.sender == CampaignRegistry(registryAddress).owner());
         _;
     }
+
+    function getCampaignData() public view returns (
+        string memory, 
+        bytes32, 
+        uint64, 
+        uint64, 
+        uint64, 
+        uint64, 
+        string memory
+    ) {
+        return (
+            campaign.identifier,
+            campaign.originPlatform,
+            campaign.goalAmount,
+            campaign.launchTime,
+            campaign.createdAt,
+            campaign.deadline,
+            campaign.creatorUrl
+        );
+    }
     
-    function getTotalPledgeAmount() public view returns(uint256 pledgedAmount) {
+    function getTotalPledgeAmount() public view returns(uint256) {
+        address tempOriginPlatform = treasuryAddress[campaign.originPlatform];
+        require(tempOriginPlatform != address(0), "CampaignInfo: Origin platform treasury not set yet");
         bytes32[] memory tempReachPlatforms = campaign.reachPlatforms;
-        uint256 length = campaign.reachPlatforms.length;
+        uint256 length = tempReachPlatforms.length;
+        uint256 pledgedAmount = CampaignTreasury(tempOriginPlatform).getPledgedAmount();
         for (uint256 i = 0; i < length; i++) {
-            if(treasuryAddresses[tempReachPlatforms[i]] != address(0)) {
+            address tempReachPlatform = treasuryAddress[tempReachPlatforms[i]];
+            if(tempReachPlatform != address(0)) {
                 pledgedAmount = pledgedAmount + 
-                CampaignTreasury(treasuryAddresses[tempReachPlatforms[i]]).getPledgeAmount();
+                CampaignTreasury(tempReachPlatform).getPledgedAmount();
             }
         }
-    }
+        return pledgedAmount;
+    }  
+
+    // function getTotalPledgeAmount() public view returns(uint256) {
+    //     uint256 pledgedAmount;
+    //     address originTreasuryAddress = treasuryAddresses[originPlatform];
+    //     address reachTreasuryAddress = treasuryAddresses[reachPlatforms];
+    //     pledgedAmount = CampaignTreasury(originTreasuryAddress).getPledgeAmount()
+    //     + CampaignTreasury(reachTreasuryAddress).getPledgeAmount();
+    //     return pledgedAmount;
+    // }
 
     function getTreasuryAddress(bytes32 clientId) public view returns(address) {
-        return treasuryAddresses[clientId];
+        require(treasuryAddress[clientId] != address(0), "CampaignInfo: Treasury address for client is not set");
+        return treasuryAddress[clientId];
     }
 
-    function editStartAt(uint64 startsAt) onlyRegistryOwner external {
-        require(startsAt + 30 days < campaign.deadline);
-        campaign.startsAt = startsAt;
+    function editLaunchTime(uint64 _launchTime) onlyRegistryOwner external {
+        require(_launchTime + 30 days < campaign.deadline);
+        campaign.launchTime = _launchTime;
     }
 
-    function editDeadline(uint64 deadline) onlyRegistryOwner external {
-        require(deadline - 30 days > campaign.startsAt);
-        campaign.deadline = deadline;
+    function editDeadline(uint64 _deadline) onlyRegistryOwner external {
+        require(_deadline - 30 days > campaign.launchTime);
+        campaign.deadline = _deadline;
     }
 
-    function setTreasuryAddress(bytes32 clientId, address treasuryAddress) onlyRegistryOwner external {
-        treasuryAddresses[clientId] = treasuryAddress;
+    function setTreasuryAddress(bytes32 _clientId, address _treasuryAddress) onlyRegistryOwner external {
+        treasuryAddress[_clientId] = _treasuryAddress;
     } 
 }
