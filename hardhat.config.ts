104b46ac862e03d65e12051227db19ad4a674fa1
rayedsikder <rayedsikder33@gmail.com>
2022-10-27 20:34:49 +0600

    Fix issue with undefined key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,14 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const PRIVATE_KEY = process.env;
+const { PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
     alfajores: {
       url: `https://alfajores-forno.celo-testnet.org`,
-      accounts: [PRIVATE_KEY]
+      accounts: [PRIVATE_KEY || ""]
     }
   }
 };
