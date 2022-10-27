28a5b6aa77424838aa1f84b3f5da8a49c5d17cf8
rayedsikder <rayedsikder33@gmail.com>
2022-10-27 13:31:36 +0600

    Fix issues with Treasury's write access

--- contracts/CampaignTreasury.sol
+++ contracts/CampaignTreasury.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import "./CampaignOracle.sol";
 import "./CampaignInfo.sol";
 
-contract CampaignTreasury is Ownable {
+contract CampaignTreasury{
     address registryAddress;
     address infoAddress;
     bytes32 public clientId;
@@ -21,17 +21,18 @@ contract CampaignTreasury is Ownable {
         clientId = _clientId;
     }
 
-    function getPledgeAmount() public view returns(uint256) {
+    function getPledgedAmount() public view returns(uint256) {
         return pledgedAmount;
     }
 
     // Old - not required by current API endpoints
     // function getCollectableByCreator() public view returns(uint256 collectableAmount) {
-    //     return CampaignOracle(oracleAddress).getPledgeAmountForClient(clientId, infoAddress) * 
+    //     return CampaignOracle(oracleAddress).getPledgedAmountForClient(clientId, infoAddress) * 
     //     feePercent / percentDivider;
     // }
 
-    function setPledgeAmount(uint256 _pledgedAmount) public onlyOwner {
+    function setPledgedAmount(uint256 _pledgedAmount) public {
+        require(CampaignRegistry(registryAddress).getOracleAddress() == msg.sender);
         pledgedAmount = _pledgedAmount;
     }
 }
