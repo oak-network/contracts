3a972471e0a9bf17d38650e6d52f248e54e08348
rayedsikder <rayedsikder33@gmail.com>
2022-10-28 10:01:42 +0600

    Add deployment configurations

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,26 @@
+import * as dotenv from "dotenv";
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const { PRIVATE_KEY } = process.env;
+dotenv.config();
+
+const { ALCHEMY_API_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
     alfajores: {
       url: `https://alfajores-forno.celo-testnet.org`,
-      accounts: [PRIVATE_KEY || ""]
+      accounts: 
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : []
+    },
+    polygon_mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: 
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : []
     }
+
   }
 };
 
