71a13896a50713308656b73cdb9f9bf53730936e
rayedsikder <rayedsikder33@gmail.com>
2022-11-16 15:33:26 +0600

    Add function for velocity of fundraising

--- contracts/testFeeSplit.sol
+++ contracts/testFeeSplit.sol
@@ -3,4 +3,13 @@ pragma solidity ^0.8.9;
 
 contract testFeeSplit {
     
+    function splitWithVelocityOfFundraising(
+        uint256 feePercent,
+        uint256 originPlatformCommissionPercent,
+        uint256 pledgedAmountByOriginPlatform,
+        uint256[] calldata pledgedAmountByReachPlatforms
+    ) public pure returns (uint256, uint256[] memory) {
+
+        return (feeShareByOriginPlatform, feeShareByReachPlatforms);
+    }    
 }
