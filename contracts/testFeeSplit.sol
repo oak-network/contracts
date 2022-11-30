9a3bdef66d23fde1a2ea15cbb7209ed17d1ebd22
rayedsikder <rayedsikder33@gmail.com>
2022-11-30 20:51:38 +0600

    Fix issue with memory and calldata

--- contracts/testFeeSplit.sol
+++ contracts/testFeeSplit.sol
@@ -8,7 +8,7 @@ contract testFeeSplit {
         uint256 feePercent,
         uint256 rewardPercent,
         bytes32[] calldata platforms,
-        uint256[] pledgedAmountByPlatformsInSequence,
+        uint256[] calldata pledgedAmountByPlatformsInSequence,
         uint256 lockTime
     ) public pure returns (bytes32, uint256, uint256[] memory) {
         uint256 regularPercent = feePercent -
