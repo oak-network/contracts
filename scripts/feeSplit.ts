f3989679cc1b0cced64fbe7300a0fb8763ef31fb
rayedsikder <rayedsikder33@gmail.com>
2022-12-01 17:38:25 +0600

    Add contract initializers for fee split

--- scripts/feeSplit.ts
+++ scripts/feeSplit.ts
@@ -19,6 +19,63 @@ async function main() {
 
     console.log(`CampaignOracle deployed to ${campaignOracle.address}`);
 
+    await campaignInfoFactory.setRegistry(campaignRegistry.address);
+    await campaignRegistry.initialize(campaignInfoFactory.address, campaignOracle.address);
+
+    const identifier = "/sampleproject";
+    const originPlatform = getHexString("Kickstarter");
+    const goalAmount = 1000000;
+    const launchTime = 1666753961;
+    const deadline = 1672002761;
+    const creatorUrl = "/samplecreatorurl/jsdkfjs";
+    const reachPlatforms = [
+        getHexString("Kickstarter"),
+        getHexString("Weirdstarter")
+    ];
+
+    const tx = await campaignInfoFactory.createCampaign(identifier, originPlatform, goalAmount, launchTime, deadline, creatorUrl, reachPlatforms);
+
+
+    const result = await tx.wait()
+    const newCampaignInfoAddress = result.events?.[1].args?.campaignInfoAddress;
+
+    console.log(`CampaignInfo created at ${newCampaignInfoAddress} using CampaignInfoFactory`);
+
+    console.log(`Deploying the Treasury for Origin platform...`);
+    const campaignTreasuryFactory = await ethers.getContractFactory("CampaignTreasury");
+    const campaignTreasury: CampaignTreasury = await campaignTreasuryFactory.deploy(
+        campaignRegistry.address,
+        newCampaignInfoAddress,
+        getHexString("Kickstarter")
+    );
+
+    console.log(`CampaignTreasury deployed to ${campaignTreasury.address}`);
+
+    const campaignInfo: CampaignInfo = await ethers.getContractAt("contracts/CampaignInfo.sol:CampaignInfo", newCampaignInfoAddress);
+
+    await campaignInfo.setTreasuryAddress(
+        getHexString("Kickstarter"),
+        campaignTreasury.address
+    );
+
+    console.log(`Treasury address set for Origin platform in CampaignInfo...`);
+
+    console.log(`Deploying the Treasury for Reach platform...`);
+    const campaignTreasury2: CampaignTreasury = await campaignTreasuryFactory.deploy(
+        campaignRegistry.address,
+        newCampaignInfoAddress,
+        getHexString("Weirdstarter")
+    );
+
+    console.log(`CampaignTreasury deployed to ${campaignTreasury2.address}`);
+
+    await campaignInfo.setTreasuryAddress(
+        getHexString("Weirdstarter"),
+        campaignTreasury2.address
+    );
+
+    console.log(`Treasury address set for reach platform in CampaignInfo`);
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
