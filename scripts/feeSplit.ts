5ba645b9008ab82abdcae4cd518d4a3ff49dea63
rayedsikder <rayedsikder33@gmail.com>
2022-12-08 19:51:25 +0600

    Fix issues with BigNumber

--- scripts/feeSplit.ts
+++ scripts/feeSplit.ts
@@ -9,6 +9,16 @@ async function main() {
     // const feeSplitLibraryFactory = await ethers.getContractFactory("FeeSplit");
     // const feeSplitLibrary: FeeSplit = await feeSplitLibraryFactory.deploy();
 
+
+    // Parameters
+    const clientWallet1 = "0xA2a6f51aF77c1bF8eB11fBE482D3e0F382105ee2";
+    const clientWallet2 = "0x63216f462174d815fc555496dD9dD5FC99395b7f";
+    const pledge1 = Number(100 * 1e18).toString()
+    const pledge2 = Number(100 * 1e18).toString()
+    const goalAmount = Number(300 * 1e18).toString()
+    const testPreMint = Number(500 * 1e18).toString()
+
+
     const campaignInfoFactoryFactory = await ethers.getContractFactory("CampaignInfoFactory");
     const campaignInfoFactory: CampaignInfoFactory = await campaignInfoFactoryFactory.deploy();
 
@@ -37,8 +47,8 @@ async function main() {
 
     console.log(`TestUSD deployed to ${testUSD.address}`);
 
-    const mint = await testUSD.mint(owner.address, ethers.utils.parseEther("100000"));
-    console.log("100000 TestUSD token minted to user");
+    const mint = await testUSD.mint(owner.address, testPreMint);
+    console.log(`${testPreMint} minted to user`);
 
     await mint.wait();
 
@@ -46,13 +56,6 @@ async function main() {
     await setRegistry.wait();
     const initialize = await campaignRegistry.initialize(campaignInfoFactory.address, campaignOracle.address);
     await initialize.wait();
-
-    // Parameters
-    const clientWallet1 = "0xA2a6f51aF77c1bF8eB11fBE482D3e0F382105ee2";
-    const clientWallet2 = "0x63216f462174d815fc555496dD9dD5FC99395b7f";
-    const pledge1 = ethers.utils.parseEther("50000");
-    const pledge2 = ethers.utils.parseEther("50000");
-    const goalAmount = ethers.utils.parseEther("110000");
 
     const identifier = "/sampleproject";
     const originPlatform = getHexString("Kickstarter");
@@ -118,22 +121,22 @@ async function main() {
     await setClientInfo.wait();
     console.log(`Treasury address set for reach platform in CampaignInfo`);
 
-    const increaseAllowance = await testUSD.increaseAllowance(campaignInfo.address, goalAmount*2);
+    const increaseAllowance = await testUSD.increaseAllowance(campaignInfo.address, goalAmount);
     await increaseAllowance.wait();
     
     //Commented the followings for demo setup
     
-    // let pledgeThroughClient = await campaignInfo.pledgeThroughClient(reachPlatforms[0], pledge1);
-    // await pledgeThroughClient.wait();
-    // console.log(`Pledged ${pledge1} to reachPlatform`);
-    // pledgeThroughClient = await campaignInfo.pledgeThroughClient(originPlatform, pledge2);
-    // await pledgeThroughClient.wait();
-    // console.log(`Pledged ${pledge2} to originPlatform`);
+    let pledgeThroughClient = await campaignInfo.pledgeThroughClient(reachPlatforms[0], pledge1);
+    await pledgeThroughClient.wait();
+    console.log(`Pledged ${pledge1} to reachPlatform`);
+    pledgeThroughClient = await campaignInfo.pledgeThroughClient(originPlatform, pledge2);
+    await pledgeThroughClient.wait();
+    console.log(`Pledged ${pledge2} to originPlatform`);
     
-    // const treasury1Balance = campaignInfo.getPledgedAmountForClientCrypto(originPlatform);
-    // console.log(`Treasury1 ${treasury1Balance}`);
-    // const treasury2Balance = campaignInfo.getPledgedAmountForClientCrypto(reachPlatforms[0]);
-    // console.log(`Treasury2 ${treasury2Balance}`);
+    const treasury1Balance = campaignInfo.getPledgedAmountForClientCrypto(originPlatform);
+    console.log(`Treasury1 ${treasury1Balance}`);
+    const treasury2Balance = campaignInfo.getPledgedAmountForClientCrypto(reachPlatforms[0]);
+    console.log(`Treasury2 ${treasury2Balance}`);
 
     // Proportional fee split with the lifecycle of fundraising
     
@@ -142,11 +145,11 @@ async function main() {
     // console.log(`Fee splits disbursed to client wallets!`);
 
     // Proportional fee split
-    // const disburseFee = await campaignInfo.disburseFee(originPlatform, 1000);
-    // await disburseFee.wait();
-    // const splitFeesProportionately = await campaignInfo.splitFeesProportionately();
-    // await splitFeesProportionately.wait();
-    // console.log(`Fee splits disbursed to client wallets!`);
+//    const disburseFee = await campaignInfo.disburseFee(originPlatform, goalAmount);
+//    await disburseFee.wait();
+    const splitFeesProportionately = await campaignInfo.splitFeesProportionately();
+    await splitFeesProportionately.wait();
+    console.log(`Fee splits disbursed to client wallets!`);
     const clientWallet1Balance = await testUSD.balanceOf(clientWallet1);
     const clientWallet2Balance = await testUSD.balanceOf(clientWallet2);
     console.log(`tUSD balance of client clientWallet ${clientWallet1Balance}`);
