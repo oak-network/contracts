6a5070c9884bf4cc8f92dc668d2fbb83f97690a4
rayedsikder <rayedsikder33@gmail.com>
2023-03-13 20:49:07 +0600

    Add structure for NFT contract

---
+++ contracts/CampaignNFT.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract CampaignNFT is ERC721, AccessControl {
+   }
