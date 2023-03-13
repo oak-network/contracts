e51c24deb2b09b15c0dcab28c962d993eec39df3
rayedsikder <rayedsikder33@gmail.com>
2023-03-13 20:49:45 +0600

    Add events and structs for CampaignNFT

--- contracts/CampaignNFT.sol
+++ contracts/CampaignNFT.sol
@@ -6,4 +6,23 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract CampaignNFT is ERC721, AccessControl {
-   }
+    using Counters for Counters.Counter;
+
+    event pledgeReceipt(
+        address indexed backer,
+        address indexed campaignInfo,
+        address token,
+        bytes32 indexed platform,
+        uint256 pledgedAmount,
+        uint256 timestamp
+    );
+
+    struct PledgeReceipt {
+        address campaignInfo;
+        address backer;
+        address token;
+        uint256 pledgedAmount;
+        uint256 timestamp;
+        bytes32 platformId;
+    }
+}
