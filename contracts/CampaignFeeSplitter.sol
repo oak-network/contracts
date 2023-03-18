a42cdfd9f4f3bea58250bbabfc42854c1d5943ed
rayedsikder <rayedsikder33@gmail.com>
2023-03-18 19:16:56 +0600

    Add CampaignFeeSplitter contract

---
+++ contracts/CampaignFeeSplitter.sol
@@ -0,0 +1,37 @@
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract CampaignFeeSplitter {
+
+    function getFeeSplitsProportionately(
+        uint256 feePercent,
+        uint256[] memory pledgedAmountByPlatforms
+    ) public pure returns (uint256[] memory) {
+        uint256 length = pledgedAmountByPlatforms.length;
+        uint256[] memory feeShareByPlatforms = new uint256[](length);
+        for (uint256 i = 0; i < length; i++) {
+            feeShareByPlatforms[i] =
+                (pledgedAmountByPlatforms[i] * feePercent) /
+                percentDivider;
+        }
+        return feeShareByPlatforms;
+    }
+
+    function getFeeSplitsProportionatelyWithPlatformReward(
+        uint256 totalFeePercent,
+        uint256 platformRewardPercent,
+        uint256 pledgedAmountByOrigin,
+        uint256[] memory pledgedAmountsByReach
+    ) public pure returns (uint256, uint256[] memory) {
+        uint256 noOfReach = pledgedAmountsByReach.length;
+        uint256 reachFeePercent = (totalFeePercent - platformRewardPercent) /
+            (noOfReach + 1);
+        uint256 feeByOrigin = (pledgedAmountByOrigin *
+            (platformRewardPercent + reachFeePercent)) / percentDivider;
+        return (
+            feeByOrigin,
+            getFeeSplitsProportionately(reachFeePercent, pledgedAmountsByReach)
+        );
+    }
+}
