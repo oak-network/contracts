e2d44e33820b222bfffcde70a05082e932dd711a
rayedsikder <rayedsikder33@gmail.com>
2023-03-18 21:59:01 +0600

    Fix issues in Fee splitter interface

--- contracts/Interface/ICampaignFeeSplitter.sol
+++ contracts/Interface/ICampaignFeeSplitter.sol
@@ -3,12 +3,14 @@ pragma solidity ^0.8.9;
 
 interface ICampaignFeeSplitter {
     function getFeeSplitsProportionately(
+        uint256 percentDivider,
         uint256 feePercent,
         uint256[] memory pledgedAmountByPlatforms
     ) external pure returns (uint256[] memory);
 
     function getFeeSplitsProportionatelyWithPlatformReward(
         uint256 totalFeePercent,
+        uint256 percentDivider,
         uint256 platformRewardPercent,
         uint256 pledgedAmountByOrigin,
         uint256[] memory pledgedAmountsByReach
