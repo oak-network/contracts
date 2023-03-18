a2fa03c3f4b09e98e36ceeda58f9335d5e18419c
rayedsikder <rayedsikder33@gmail.com>
2023-03-18 19:30:19 +0600

    Add interface ICampaignRegistry

---
+++ contracts/Interface/ICampaignRegistry.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+interface ICampaignRegistry {
+    function initialize(
+        address _factoryAddress,
+        address _oracleAddress,
+        address _campaignNFTAddress,
+        address _campaignGlobalParemeters, 
+        address _campaignFeeSplitter
+    ) external;
+
+    function getOracleAddress() external view returns (address);
+
+    function getFactoryAddress() external view returns (address);
+
+    function getCampaignNFTAddress() external view returns (address);
+
+    function getCampaignGlobalParameters() external view returns (address);
+
+    function getCampaignFeeSplitter() external view returns (address);
+
+    function getCampaignInfoAddress(string calldata identifier)
+        external
+        view
+        returns (address);
+
+    function setCampaignInfoAddress(
+        string calldata _identifier,
+        address _campaignAddress
+    ) external;
+}
