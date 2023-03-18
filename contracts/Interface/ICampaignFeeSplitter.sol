cc8af630e58ee4ecde0508b0f556d26c34c4318c
rayedsikder <rayedsikder33@gmail.com>
2023-03-18 19:16:36 +0600

    Add interface ICampaignFeeSplitter

---
+++ contracts/Interface/ICampaignFeeSplitter.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+interface ICampaignFeeSplitter {
+    function getFeeSplitsProportionately(
+        uint256 feePercent,
+        uint256[] memory pledgedAmountByPlatforms
+    ) external pure returns (uint256[] memory);
+
+    function getFeeSplitsProportionatelyWithPlatformReward(
+        uint256 totalFeePercent,
+        uint256 platformRewardPercent,
+        uint256 pledgedAmountByOrigin,
+        uint256[] memory pledgedAmountsByReach
+    ) external pure returns (uint256, uint256[] memory);
+}
