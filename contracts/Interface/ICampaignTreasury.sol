c0e8e29e803cc950849759b265e487e1402a4c12
rayedsikder <rayedsikder33@gmail.com>
2023-03-18 14:41:39 +0600

    Add interface for CampaignTreasury

---
+++ contracts/Interface/ICampaignTreasury.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface ICampaignTreasury {
+    function getplatformId() external view returns (bytes32);
+
+    function getplatformFeePercent() external view returns (uint256);
+
+    function getplatformFee() external view returns (uint256);
+
+    function getTotalCollectableByCreator() external view returns (uint256);
+
+    function getPledgedAmount() external view returns (uint256);
+
+    function setplatformFeePercent(uint256 _platformFeePercent) external;
+
+    function setPledgedAmount(uint256 _pledgedAmount) external;
+
+    function disburseFeeToPlatform(
+        address _platform,
+        address _token,
+        uint256 _amount
+    ) external;
+}
