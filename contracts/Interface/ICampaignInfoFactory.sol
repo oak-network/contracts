4e1c34753b6c9f38bf2c5f662a03f4a90e43bb31
rayedsikder <rayedsikder33@gmail.com>
2023-04-06 01:56:20 +0600

    Add interface

---
+++ contracts/Interface/ICampaignInfoFactory.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+interface ICampaignInfoFactory {
+    function createCampaign(
+        address _creator,
+        string memory _identifier,
+        bytes32 _originPlatform,
+        string memory _creatorUrl,
+        bytes32[] memory _reachPlatform,
+        uint256 _earlyPledgeAmount
+    ) external;
+}
