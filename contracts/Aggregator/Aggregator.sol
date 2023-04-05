a4e3e89c8a9e7b1689c72e6737fb36d0a17f4954
rayedsikder <rayedsikder33@gmail.com>
2023-04-06 04:32:59 +0600

    Add function setTreasury

--- contracts/Aggregator/Aggregator.sol
+++ contracts/Aggregator/Aggregator.sol
@@ -3,12 +3,23 @@ pragma solidity ^0.8.9;
 
 import "../Interface/ICampaignInfoFactory.sol";
 import "../Interface/ICampaignRegistry.sol";
-
+import "../Interface/ICampaignInfo.sol";
+import "../CampaignTreasury.sol";
 
 contract Aggregator {
     mapping(address => address) campaignOwners;
     address campaignInfoFactory;
     address campaignRegistry;
+
+    CampaignTreasury newTreasury;
+
+    function initialize(
+        address _campaignInfoFactory,
+        address _campaignRegisty
+    ) external {
+        campaignInfoFactory = _campaignInfoFactory;
+        campaignRegistry = _campaignRegisty;
+    }
 
     function createCampaign(
         address _creator,
@@ -18,10 +29,36 @@ contract Aggregator {
         bytes32[] memory _reachPlatform,
         uint256 _earlyPledgeAmount
     ) external {
-        campaignOwners[]
-        ICampaignInfoFactory(campaignInfoFactory).createCampaign(_creator, _identifier, _originPlatform, _creatorUrl, _reachPlatform, _earlyPledgeAmount);
-        address infoAddress = ICampaignRegistry(campaignRegisty).getCampaignInfoAddress();
+        ICampaignInfoFactory(campaignInfoFactory).createCampaign(
+            _creator,
+            _identifier,
+            _originPlatform,
+            _creatorUrl,
+            _reachPlatform,
+            _earlyPledgeAmount
+        );
+        address infoAddress = ICampaignRegistry(campaignRegistry)
+            .getCampaignInfoAddress(_identifier);
         campaignOwners[infoAddress] = msg.sender;
     }
 
+    function setTreasury(
+        address campaignInfo,
+        bytes32 platform,
+        address platformWallet,
+        address token
+    ) external {
+        newTreasury = new CampaignTreasury(
+            campaignRegistry,
+            campaignInfo,
+            platform
+        );
+        address treasury = address(newTreasury);
+        ICampaignInfo(campaignInfo).setPlatformInfo(
+            platform,
+            platformWallet,
+            treasury,
+            token
+        );
+    }
 }
