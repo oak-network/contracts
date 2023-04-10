4c2e2b297ef891fb5030c0fa63f59d98881e7d52
rayedsikder <rayedsikder33@gmail.com>
2023-04-11 02:33:24 +0600

    Add functions and fix issue with creator access

--- contracts/Aggregator/Aggregator.sol
+++ contracts/Aggregator/Aggregator.sol
@@ -21,6 +21,11 @@ contract Aggregator {
         campaignRegistry = _campaignRegisty;
     }
 
+    modifier onlyCampaignOwner(address campaignInfo) {
+        require(campaignOwners[campaignInfo] == msg.sender);
+        _;
+    }
+
     function createCampaign(
         address _creator,
         string memory _identifier,
@@ -30,7 +35,7 @@ contract Aggregator {
         uint256 _earlyPledgeAmount
     ) external {
         ICampaignInfoFactory(campaignInfoFactory).createCampaign(
-            _creator,
+            address(this),
             _identifier,
             _originPlatform,
             _creatorUrl,
@@ -39,7 +44,7 @@ contract Aggregator {
         );
         address infoAddress = ICampaignRegistry(campaignRegistry)
             .getCampaignInfoAddress(_identifier);
-        campaignOwners[infoAddress] = msg.sender;
+        campaignOwners[infoAddress] = _creator;
     }
 
     function setTreasury(
@@ -47,7 +52,7 @@ contract Aggregator {
         bytes32 platform,
         address platformWallet,
         address token
-    ) external {
+    ) external onlyCampaignOwner(campaignInfo) {
         newTreasury = new CampaignTreasury(
             campaignRegistry,
             campaignInfo,
@@ -59,6 +64,62 @@ contract Aggregator {
             platformWallet,
             treasury,
             token
+        );
+    }
+
+    function setLaunch(
+        address campaignInfo,
+        uint256 launchTime,
+        uint256 deadline,
+        uint256 goalAmount,
+        bool enableLatePledge
+    ) external onlyCampaignOwner(campaignInfo) {
+        ICampaignInfo(campaignInfo).setLaunch(
+            launchTime,
+            deadline,
+            goalAmount,
+            enableLatePledge
+        );
+    }
+
+    function addItem(
+        address campaignInfo,
+        string calldata _itemId,
+        string calldata _description
+    ) external onlyCampaignOwner(campaignInfo) {
+        ICampaignInfo(campaignInfo).addItem(_itemId, _description);
+    }
+
+    function addReward(
+        address campaignInfo,
+        string calldata name,
+        uint256 rewardValue,
+        string[] memory itemName,
+        uint256[] memory itemQuantity
+    ) external onlyCampaignOwner(campaignInfo) {
+        ICampaignInfo(campaignInfo).addReward(
+            name,
+            rewardValue,
+            itemName,
+            itemQuantity
+        );
+    }
+
+    function addItemAndReward(
+        address campaignInfo,
+        string calldata itemId,
+        string calldata description,
+        string calldata name,
+        uint256 rewardValue,
+        string[] memory itemName,
+        uint256[] memory itemQuantity
+    ) external onlyCampaignOwner(campaignInfo) {
+        ICampaignInfo(campaignInfo).addItem(itemId, description);
+        ICampaignInfo(campaignInfo).addReward(
+            name,
+            rewardValue,
+            itemName,
+            itemQuantity
         );
     }
 }
