9df8c0c280b767606cadd424d81252580bbd2bc8
rayedsikder <rayedsikder33@gmail.com>
2023-05-29 21:18:09 +0600

    Update token with permit

--- contracts/testUSD.sol
+++ contracts/testUSD.sol
@@ -1,11 +1,12 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TestUSD is ERC20, Ownable {
-    constructor() ERC20("testUSD", "tUSD") {}
+contract TestUSD is ERC20, ERC20Permit, Ownable {
+    constructor() ERC20("testUSD", "tUSD") ERC20Permit("testUSD") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
