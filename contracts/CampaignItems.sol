41c955ca7046458324324721e547e370ad8820e5
rayedsikder <rayedsikder33@gmail.com>
2023-06-07 13:43:11 +0600

    Add Containers structure and functions

--- contracts/CampaignItems.sol
+++ contracts/CampaignItems.sol
@@ -2,5 +2,22 @@
 pragma solidity ^0.8.9;
 
 contract CampaignItems {
-    
+    struct Container {
+        uint256[] items;
+        uint256[] itemQuantity;
+        uint256 value;
+        bool isRewardTier;
+    }
+
+    mapping(bytes32 => Container) containers;
+    mapping(address => bytes32) containerOwners;
+
+    function addContainer(
+        address creator,
+        bytes32 id,
+        Container memory container
+    ) public {
+        containers[id] = container;
+        containerOwners[creator] = id;
+    }
 }
