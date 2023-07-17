1ed582859b9aa518160fab78760a77e421acf767
rayedsikder <rayedsikder33@gmail.com>
2023-07-18 05:56:20 +0600

    Update Info interface for model

--- contracts/Interface/ICampaignInfo.sol
+++ contracts/Interface/ICampaignInfo.sol
@@ -8,40 +8,35 @@ interface ICampaignInfo {
 
     function totalRaisedBalance() external view returns (uint256);
 
-    function treasuryAddress(bytes32 platform) external view returns (address);
+    function treasury(bytes32 platform) external view returns (address);
 
     function token() external view returns (address);
 
-    function feeSplitModel() external view returns (bytes32);
+    // function feeSplitModel() external view returns (bytes32);
 
-    function launchTime() external view returns (uint256);
+    // function launchTime() external view returns (uint256);
 
     function deadline() external view returns (uint256);
 
+    function goal() external view returns (uint256);
+
     function platforms() external view returns (bytes32[] memory);
 
-    function getCampaignData()
-        external
-        view
-        returns (string memory, uint256, uint256, uint256, string memory);
+    function creator() external view returns (address);
 
-    function claimFee(bytes32 platform) external;
+    // function claimFee(bytes32 platform) external;
 
-    function setPlatformInfo(
-        bytes32 _platformId,
-        address _treasury,
-        address _token
-    ) external;
+    function setPlatformInfo(bytes32 _platformId, address _treasury) external;
 
-    function updateLaunchTime(uint256 _launchTime) external;
+    // function updateLaunchTime(uint256 _launchTime) external;
 
-    function updateDeadline(uint256 _deadline) external;
+    // function updateDeadline(uint256 _deadline) external;
 
-    function updateGoal(uint256 _goalAmount) external;
+    // function updateGoal(uint256 _goalAmount) external;
 
-    function pause() external;
+    // function pause() external;
 
-    function unpause() external;
+    // function unpause() external;
 
     function transferOwnership(address newOwner) external;
 }
