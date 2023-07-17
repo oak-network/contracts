9c8011c5b00091f9819644d08bd6b39f915011f8
rayedsikder <rayedsikder33@gmail.com>
2023-07-17 13:48:29 +0600

    Update treasury interface

--- contracts/Interface/ICampaignTreasury.sol
+++ contracts/Interface/ICampaignTreasury.sol
@@ -4,25 +4,30 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface ICampaignTreasury {
+    
     function getplatformId() external view returns (bytes32);
 
     function getplatformFeePercent() external view returns (uint256);
 
     function getplatformFee() external view returns (uint256);
 
-    function getTotalCollectableByCreator() external view returns (uint256);
+    // function getTotalCollectableByCreator() external view returns (uint256);
 
-    function getPledgedAmount() external view returns (uint256);
+    function raisedBalance() external view returns (uint256);
 
-    function pledgeInFiat(uint256 amount) external;
+    function currentBalance() external view returns (uint256);
 
-    function setplatformFeePercent(uint256 _platformFeePercent) external;
+    // function getPledgedAmount() external view returns (uint256);
 
-    function setPledgedAmount(uint256 _pledgedAmount) external;
+    // function pledgeInFiat(uint256 amount) external;
 
-    function disburseFeeToPlatform(
-        address _platform,
-        address _token,
-        uint256 _amount
-    ) external;
+    // function setplatformFeePercent(uint256 _platformFeePercent) external;
+
+    // function setPledgedAmount(uint256 _pledgedAmount) external;
+
+    // function disburseFeeToPlatform(
+    //     address _platform,
+    //     address _token,
+    //     uint256 _amount
+    // ) external;
 }
