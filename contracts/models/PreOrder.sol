3b12df8ec5299e5c76019a37bdf26edca36ba218
rayedsikder <rayedsikder33@gmail.com>
2023-08-22 12:55:56 +0600

    Add contract for PreOrder

---
+++ contracts/models/PreOrder.sol
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "../Interface/ICampaignTreasury.sol";
+import "../Interface/ICampaignInfo.sol";
+
+contract PreOrder is ICampaignTreasury, ERC721Burnable {
+    using Counters for Counters.Counter;
+
+    address public immutable registry;
+    address public immutable info;
+    bytes32 public immutable platform;
+    uint256 constant percentDivider = 10000;
+    uint256 public pledgedAmount;
+    uint256 public platformFeePercent;
+    uint256 public raisedBalance;
+    mapping(uint256 => uint256) tokenToPledgeAmount;
+
+    event receipt(
+        address indexed backer,
+        bytes32 indexed reward,
+        uint256 pledgeAmount,
+        uint256 tokenId,
+        bool isPreLaunchPledge,
+        bytes32[] rewards
+    );
+
+    Counters.Counter private _tokenIdCounter;
+
+
+
+    function pledge(
+        address backer,
+        bytes32[] calldata reward,
+        uint256 amount
+    ) external {
+        uint256 tokenId = _tokenIdCounter.current();
+        ICampaignInfo campaign = ICampaignInfo(info);
+        address token = campaign.token();
+        uint256 pledgeAmount = 0;
+        bool isPreLaunchPledge;
+        bool success;
+        require(block.timestamp <= campaign.deadline(), "AllOrNothing: Deadline reached");
+        if (block.timestamp > campaign.launchTime()) {
+            if (reward[0] != 0x00) {
+                // uint256 value = rewards[reward[0]].rewardValue;
+                bool isRewardTier = rewards[reward[0]].isRewardTier;
+                require(isRewardTier == true);
+                uint256 rewardLen = reward.length;
+                uint256 totalValue;
+                for (uint256 i = 0; i < rewardLen; i++) {
+                    totalValue += rewards[reward[i]].rewardValue;
+                }
+                success = IERC20(token).transferFrom(backer, address(this), totalValue);
+                require(success);
+                pledgeAmount = totalValue;
+                _tokenIdCounter.increment();
+                _safeMint(
+                    backer,
+                    tokenId
+                    // ,
+                    // abi.encodePacked(backer, " ", reward[0])
+                );
+                tokenToPledgeAmount[tokenId] = totalValue;
+            } else {
+                success = IERC20(token).transferFrom(backer, address(this), amount);
+                require(success);
+                pledgeAmount = amount;
+            }
+        } else {
+            isPreLaunchPledge = true;
+            success = IERC20(token).transferFrom(backer, address(this), preLaunchPledge);
+            require(success);
+            pledgeAmount = preLaunchPledge;
+            _tokenIdCounter.increment();
+            _safeMint(
+                backer,
+                tokenId
+                // ,
+                // abi.encodePacked(backer, " PreLaunchPledge")
+            );
+        }
+        raisedBalance += pledgeAmount;
+        emit receipt(
+            backer,
+            reward[0],
+            pledgeAmount,
+            tokenId,
+            isPreLaunchPledge,
+            reward
+        );
+    }
+
+    function collect() external {
+        ICampaignInfo campaign = ICampaignInfo(info);
+        require(block.timestamp >= campaign.deadline());
+        uint256 balance = currentBalance();
+        require(balance >= campaign.goal() / campaign.platforms().length);
+        IERC20(campaign.token()).transfer(campaign.creator(), balance);
+    }
+
+    function claimRefund(uint256 tokenId) external {
+        uint256 amount = tokenToPledgeAmount[tokenId];
+        address token = ICampaignInfo(info).token();
+        require(amount != 0, "AllOrNothing: PreLaunch pledge");
+        tokenToPledgeAmount[tokenId] = 0;
+        burn(tokenId);
+        bool success = IERC20(token).transfer(_msgSender(), amount);
+        require(success);
+    }
+
+    // The following functions are overrides required by Solidity.
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function getplatformId() external view returns (bytes32) {
+        return platform;
+    }
+
+    function getplatformFeePercent() external view returns (uint256) {
+        return platformFeePercent;
+    }
+
+    function getplatformFee() external view returns (uint256) {
+        return (pledgedAmount * platformFeePercent) / percentDivider;
+    }
+
+    function setplatformFeePercent(uint256 _platformFeePercent) external {
+        platformFeePercent = _platformFeePercent;
+    }
+
+    function currentBalance() public view override returns (uint256) {
+        return IERC20(ICampaignInfo(info).token()).balanceOf(address(this));
+    }
+}
