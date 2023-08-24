8ae1ec86ab7d8824c8da3f4d0fe69c846c969bfd
rayedsikder <rayedsikder33@gmail.com>
2023-08-24 13:49:19 +0600

    Add custom error for 0 and implement in modifiers

--- contracts/GlobalParams.sol
+++ contracts/GlobalParams.sol
@@ -3,21 +3,41 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
-   
-contract CampaignInfo is Ownable, Pausable {
+
+contract GlobalParams is Ownable, Pausable {
+
+    error GlobalParamsInvalidAddress(address account);
+
     address public protocolAdminAddress;
     address public tokenAddress;
     uint256 public protocolFeePercent;
     mapping(bytes32 => bool) private platformIsListed;
     mapping(bytes32 => address) private platformAdminAddress;
 
-    constructor(address _protocolAdminAddress, address _tokenAddress, uint256 _protocolFeePercent) {
+    constructor(
+        address _protocolAdminAddress,
+        address _tokenAddress,
+        uint256 _protocolFeePercent
+    ) {
         protocolAdminAddress = _protocolAdminAddress;
         tokenAddress = _tokenAddress;
         protocolFeePercent = _protocolFeePercent;
     }
 
-    function updateProtocolAdminAddress(address _protocolAdminAddress) external onlyOwner {
+    modifier notAddressZero(address _account) {
+        _checkIfAddressZero(_account);
+        _;
+    }
+
+    function _checkIfAddressZero(address _account) internal pure {
+        if (_account == address(0)) {
+            revert GlobalParamsInvalidAddress(_account);
+        }
+    }
+
+    function updateProtocolAdminAddress(
+        address _protocolAdminAddress
+    ) external onlyOwner {
         protocolAdminAddress = _protocolAdminAddress;
     }
 
@@ -25,11 +45,16 @@ contract CampaignInfo is Ownable, Pausable {
         tokenAddress = _tokenAddress;
     }
 
-    function updateProtocolFeePercent(uint256 _protocolFeePercent) external onlyOwner {
+    function updateProtocolFeePercent(
+        uint256 _protocolFeePercent
+    ) external onlyOwner {
         protocolFeePercent = _protocolFeePercent;
     }
 
-    function updatePlatformAdminAddress(bytes32 _platformBytes, address _platformAdminAddress) external onlyOwner {
+    function updatePlatformAdminAddress(
+        bytes32 _platformBytes,
+        address _platformAdminAddress
+    ) external onlyOwner {
         platformAdminAddress[_platformBytes] = _platformAdminAddress;
     }
 }
