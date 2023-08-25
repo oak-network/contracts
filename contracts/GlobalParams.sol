8c244f08278dd99aac8358e3e433be1e3d958490
rayedsikder <rayedsikder33@gmail.com>
2023-08-25 19:21:12 +0600

    Add feature to enlist & delist a platform

--- contracts/GlobalParams.sol
+++ contracts/GlobalParams.sol
@@ -3,14 +3,22 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract GlobalParams is Ownable, Pausable {
+import "./Interface/IGlobalParams.sol";
+
+contract GlobalParams is IGlobalParams, Ownable, Pausable {
+    using Counters for Counters.Counter;
+
     error GlobalParamsInvalidAddress(address account);
     error GlobalParamsPlatformNotListed(
         bytes32 platformBytes,
         address platformAdminAddress
     );
+    error GlobalParamsPlatformAlreadyListed(bytes32 platformBytes);
     error GlobalParamsPlatformAdminNotSet(bytes32 platformBytes);
+
+    Counters.Counter private numberOfListedPlatforms;
 
     address public protocolAdminAddress;
     address public tokenAddress;
@@ -35,7 +43,7 @@ contract GlobalParams is Ownable, Pausable {
 
     function checkIfplatformIsListed(
         bytes32 _platformBytes
-    ) external view returns (bool) {
+    ) external view override returns (bool) {
         if (platformIsListed[_platformBytes]) {
             return true;
         } else return false;
@@ -43,7 +51,7 @@ contract GlobalParams is Ownable, Pausable {
 
     function getPlatformAdminAddress(
         bytes32 _platformBytes
-    ) external view returns (address account) {
+    ) external view override returns (address account) {
         account = platformAdminAddress[_platformBytes];
         if (account == address(0)) {
             revert GlobalParamsPlatformAdminNotSet(_platformBytes);
@@ -56,28 +64,51 @@ contract GlobalParams is Ownable, Pausable {
         }
     }
 
+    function enlistPlatform(
+        bytes32 _platformBytes,
+        address _platformAdminAddress
+    ) external {
+        if (platformIsListed[_platformBytes]) {
+            revert GlobalParamsPlatformAlreadyListed(_platformBytes);
+        } else {
+            platformIsListed[_platformBytes] = true;
+            platformAdminAddress[_platformBytes] = _platformAdminAddress;
+            numberOfListedPlatforms.increment();
+        }
+    }
+
+    function delistPlatform(bytes32 _platformBytes) external {
+        if (platformIsListed[_platformBytes]) {
+            platformIsListed[_platformBytes] = false;
+            platformAdminAddress[_platformBytes] = address(0);
+            numberOfListedPlatforms.decrement();
+        } else {
+            revert GlobalParamsPlatformNotListed(_platformBytes, address(0));
+        }
+    }
+
     function updateProtocolAdminAddress(
         address _protocolAdminAddress
-    ) external onlyOwner notAddressZero(_protocolAdminAddress) {
+    ) external override onlyOwner notAddressZero(_protocolAdminAddress) {
         protocolAdminAddress = _protocolAdminAddress;
     }
 
     function updateTokenAddress(
         address _tokenAddress
-    ) external onlyOwner notAddressZero(_tokenAddress) {
+    ) external override onlyOwner notAddressZero(_tokenAddress) {
         tokenAddress = _tokenAddress;
     }
 
     function updateProtocolFeePercent(
         uint256 _protocolFeePercent
-    ) external onlyOwner {
+    ) external override onlyOwner {
         protocolFeePercent = _protocolFeePercent;
     }
 
     function updatePlatformAdminAddress(
         bytes32 _platformBytes,
         address _platformAdminAddress
-    ) external onlyOwner {
+    ) external override onlyOwner notAddressZero(_platformAdminAddress) {
         if (platformIsListed[_platformBytes]) {
             platformAdminAddress[_platformBytes] = _platformAdminAddress;
         } else
