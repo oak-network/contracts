1e93229a4824cfc9f8fc7f1d68ceed6bb05ea28b
rayedsikder <rayedsikder33@gmail.com>
2023-08-29 18:03:02 +0600

    Update ICampaignTreasury

--- contracts/interfaces/ICampaignTreasury.sol
+++ contracts/interfaces/ICampaignTreasury.sol
@@ -1,33 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
 interface ICampaignTreasury {
-    
-    function getplatformId() external view returns (bytes32);
-
+    function disburseFees() external;
+    function withdrawCrptoPledgedAmount() external;
+    function claimRefund(uint256 tokenId) external;
+    function getplatformBytes() external view returns (bytes32);
     function getplatformFeePercent() external view returns (uint256);
-
-    function getplatformFee() external view returns (uint256);
-
-    // function getTotalCollectableByCreator() external view returns (uint256);
-
-    function raisedBalance() external view returns (uint256);
-
-    function currentBalance() external view returns (uint256);
-
-    // function getPledgedAmount() external view returns (uint256);
-
-    // function pledgeInFiat(uint256 amount) external;
-
-    // function setplatformFeePercent(uint256 _platformFeePercent) external;
-
-    // function setPledgedAmount(uint256 _pledgedAmount) external;
-
-    // function disburseFeeToPlatform(
-    //     address _platform,
-    //     address _token,
-    //     uint256 _amount
-    // ) external;
+    function getRaisedAmount() external view returns (uint256);
 }
