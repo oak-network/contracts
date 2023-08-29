c59ba9a6e5f1b02edd7fa6911337e773ae479074
rayedsikder <rayedsikder33@gmail.com>
2023-08-29 09:46:48 +0600

    Add function to check if time is within range

--- contracts/utils/TimestampChecker.sol
+++ contracts/utils/TimestampChecker.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.9;
 
 abstract contract TimestampChecker {
-
     error CurrentTimeIsGreater(uint256 inputTime, uint256 currentTime);
     error CurrentTimeIsLess(uint256 inputTime, uint256 currentTime);
+    error CurrentTimeIsNotWithinRange(uint256 initialTime, uint256 finalTime);
 
     modifier currentTimeIsGreater(uint256 inputTime) {
         _checkIfCurrentTimeIsGreater(inputTime);
@@ -16,18 +16,36 @@ abstract contract TimestampChecker {
         _;
     }
 
-    function _checkIfCurrentTimeIsGreater(uint256 inputTime) internal view virtual {
+    modifier currentTimeIsWithinRange(uint256 initialTime, uint256 finalTime) {
+        _checkIfCurrentTimeIsWithinRange(initialTime, finalTime);
+        _;
+    }
+
+    function _checkIfCurrentTimeIsGreater(
+        uint256 inputTime
+    ) internal view virtual {
         uint256 currentTime = block.timestamp;
         if (currentTime > inputTime) {
             revert CurrentTimeIsGreater(inputTime, currentTime);
         }
     }
 
-    function _checkIfCurrentTimeIsLess(uint256 inputTime) internal view virtual {
+    function _checkIfCurrentTimeIsLess(
+        uint256 inputTime
+    ) internal view virtual {
         uint256 currentTime = block.timestamp;
         if (currentTime < inputTime) {
             revert CurrentTimeIsLess(inputTime, currentTime);
         }
     }
+
+    function _checkIfCurrentTimeIsWithinRange(
+        uint256 initialTime,
+        uint256 finalTime
+    ) internal view virtual {
+        uint256 currentTime = block.timestamp;
+        if (currentTime < initialTime || currentTime > finalTime) {
+            revert CurrentTimeIsNotWithinRange(initialTime, finalTime);
+        }
+    }
 }
-    
