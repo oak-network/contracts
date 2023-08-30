181d0350573ed41d5d19486904a1f4a17417a36b
rayedsikder <rayedsikder33@gmail.com>
2023-08-30 19:23:24 +0600

    Add events for fiat transactions

--- contracts/utils/FiatEnabled.sol
+++ contracts/utils/FiatEnabled.sol
@@ -6,25 +6,45 @@ abstract contract FiatEnabled {
     bool internal s_fiatFeeIsDisbursed;
     mapping(bytes32 => uint256) internal s_fiatAmountById;
 
+    event FiatTransactionUpdated(
+        bytes32 indexed fiatTransactionId,
+        uint256 fiatTransactionAmount
+    );
+
+    event FiatFeeDisbusementStateUpdated(
+        bool isDisbursed,
+        uint256 protocolFeeAmount,
+        uint256 platformFeeAmount
+    );
+
     error FiatEnabledAlreadySet();
     error FiatEnabledDisallowedState();
 
-    function _updateFiatPledge(
-        bytes32 fiatPledgeId,
-        uint256 fiatPledgeAmount
+    function _updateFiatTransaction(
+        bytes32 fiatTransactionId,
+        uint256 fiatTransactionAmount
     ) internal {
-        s_fiatAmountById[fiatPledgeId] = fiatPledgeAmount;
-        s_fiatRaisedAmount += fiatPledgeAmount;
+        s_fiatAmountById[fiatTransactionId] = fiatTransactionAmount;
+        s_fiatRaisedAmount += fiatTransactionAmount;
+        emit FiatTransactionUpdated(fiatTransactionId, fiatTransactionAmount);
     }
 
-    function _updateFiatFeeDisbusementState(bool isDisbursed) internal {
+    function _updateFiatFeeDisbusementState(
+        bool isDisbursed,
+        uint256 protocolFeeAmount,
+        uint256 platformFeeAmount
+    ) internal {
         if (s_fiatFeeIsDisbursed == true) {
             revert FiatEnabledAlreadySet();
         }
         if (isDisbursed) {
             s_fiatFeeIsDisbursed = true;
-        }
-        else {
+            emit FiatFeeDisbusementStateUpdated(
+                isDisbursed,
+                protocolFeeAmount,
+                platformFeeAmount
+            );
+        } else {
             revert FiatEnabledDisallowedState();
         }
     }
