60f590c7eeac26543b5d982a38f7e4f58d686362
rayedsikder <rayedsikder33@gmail.com>
2023-08-30 18:06:44 +0600

    Update interface according to implementation

--- contracts/interfaces/ICampaignInfo.sol
+++ contracts/interfaces/ICampaignInfo.sol
@@ -3,20 +3,50 @@ pragma solidity ^0.8.9;
 
 interface ICampaignInfo {
     function owner() external view returns (address);
-    function checkIfPlatformSelected(bytes32 platformBytes) external view returns (bool);
+
+    function checkIfPlatformSelected(
+        bytes32 platformBytes
+    ) external view returns (bool);
+
     function getTotalRaisedAmount() external view returns (uint256);
+
     function getProtocolAdminAddress() external view returns (address);
-    function getPlatformAdminAddress(bytes32 platformBytes) external view returns (address);
+
+    function getPlatformAdminAddress(
+        bytes32 platformBytes
+    ) external view returns (address);
+
     function getLaunchTime() external view returns (uint256);
+
     function getDeadline() external view returns (uint256);
+
     function getGoalAmount() external view returns (uint256);
+
     function getTokenAddress() external view returns (address);
+
     function getProtocolFeePercent() external view returns (uint256);
+
+    function getPlatformFeePercent(
+        bytes32 platformBytes
+    ) external view returns (uint256);
+
     function getIdentifierHash() external view returns (bytes32);
-    function setPlatformInfo(bytes32 platformBytes, address platformTreasuryAddress) external;
+
+    function setPlatformInfo(
+        bytes32 platformBytes,
+        address platformTreasuryAddress
+    ) external;
+
     function transferOwnership(address newOwner) external;
+
     function updateLaunchTime(uint256 launchTime) external;
+
     function updateDeadline(uint256 deadline) external;
+
     function updateGoalAmount(uint256 goalAmount) external;
-    function updateSelectedPlatform(bytes32 platformBytes, bool selection) external;
+
+    function updateSelectedPlatform(
+        bytes32 platformBytes,
+        bool selection
+    ) external;
 }
--- contracts/interfaces/ICampaignTreasury.sol
+++ contracts/interfaces/ICampaignTreasury.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.9;
 
 interface ICampaignTreasury {
     function disburseFees() external;
-    function withdrawCrptoPledgedAmount() external;
+    function withdraw() external;
     function claimRefund(uint256 tokenId) external;
     function getplatformBytes() external view returns (bytes32);
     function getplatformFeePercent() external view returns (uint256);
