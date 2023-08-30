7d4606b8c99a6e4b34467f423da2c5aa02fb35d6
rayedsikder <rayedsikder33@gmail.com>
2023-08-30 17:00:14 +0600

    Rearrange PreOrder structure and add goalChecks

--- contracts/models/PreOrder.sol
+++ contracts/models/PreOrder.sol
@@ -2,26 +2,33 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "../Interface/ICampaignTreasury.sol";
-import "../Interface/ICampaignInfo.sol";
+import "../interfaces/ICampaignInfo.sol";
+import "../utils/BasicTreasury.sol";
+import "../utils/TimestampChecker.sol";
 
-contract PreOrder is ICampaignTreasury, ERC721Burnable {
+contract PreOrder is BasicTreasury, ERC721Burnable, TimestampChecker {
     using Counters for Counters.Counter;
 
-    address public immutable registry;
-    address public immutable info;
-    bytes32 public immutable platform;
-    uint256 public immutable minimumPledgeCount;
-    uint256 constant percentDivider = 10000;
-    uint256 public pledgedAmount;
-    uint256 public platformFeePercent;
-    uint256 public raisedBalance;
-    uint256 public pledgeCount;
-    address constant platformAddress = 0x9Aee2Bb8906D3f3B1BB957765eb76a880bc47788;
-    address constant protocolAddress = 0x9Aee2Bb8906D3f3B1BB957765eb76a880bc47788;
-    mapping(uint256 => uint256) tokenToPledgeAmount;
+    struct Reward {
+        uint256 rewardValue;
+        bytes32[] itemId;
+        uint256[] itemValue;
+        uint256[] itemQuantity;
+    }
+
+    uint256 private constant MINIMUM_NO_OF_ORDER_REQUIRED = 1000;
+    uint256 private s_orderValueAmount;
+    uint256 public s_platformFeePercent;
+
+    mapping(uint256 => uint256) private s_tokenToPledgedAmount;
+
+    Counters.Counter private s_tokenIdCounter;
+    Counters.Counter private s_numberOfOrders;
+
+    mapping(bytes32 => Reward) rewards;
 
     event receipt(
         address indexed backer,
@@ -30,26 +37,20 @@ contract PreOrder is ICampaignTreasury, ERC721Burnable {
         uint256 tokenId
     );
 
-    Counters.Counter private _tokenIdCounter;
-
-    struct Reward {
-        uint256 rewardValue;
-        bytes32[] itemId;
-        uint256[] itemValue;
-        uint256[] itemQuantity;
-    }
-    mapping(bytes32 => Reward) rewards;
-
     constructor(
-        address _registry,
-        address _info,
-        bytes32 _platform,
-        uint256 _minimumPledgeCount
-    ) ERC721("CampaignNFT", "CNFT") {
-        registry = _registry;
-        info = _info;
-        platform = _platform;
-        minimumPledgeCount = _minimumPledgeCount;
+        bytes32 platformBytes,
+        address infoAddress,
+        address tokenAddress,
+        uint256 platformFeePercent
+    )
+        ERC721("", "")
+        BasicTreasury(
+            platformBytes,
+            platformFeePercent,
+            infoAddress,
+            tokenAddress
+        )
+    {
     }
 
     function getReward(
@@ -63,7 +64,7 @@ contract PreOrder is ICampaignTreasury, ERC721Burnable {
     }
 
     function pledge(address backer, bytes32 rewardName) external {
-        uint256 tokenId = _tokenIdCounter.current();
+        uint256 tokenId = s_tokenIdCounter.current();
         ICampaignInfo campaign = ICampaignInfo(info);
         address token = campaign.token();
         require(
@@ -78,7 +79,7 @@ contract PreOrder is ICampaignTreasury, ERC721Burnable {
             rewardValue
         );
         require(success);
-        _tokenIdCounter.increment();
+        s_tokenIdCounter.increment();
         _safeMint(
             backer,
             tokenId
@@ -87,25 +88,8 @@ contract PreOrder is ICampaignTreasury, ERC721Burnable {
         );
         raisedBalance += rewardValue;
         tokenToPledgeAmount[tokenId] = rewardValue;
-        pledgeCount++;
+        noOfPledges.increment();
         emit receipt(backer, rewardName, rewardValue, tokenId);
-    }
-
-    function collect() external {
-        ICampaignInfo campaign = ICampaignInfo(info);
-        address token = campaign.token();
-        require(block.timestamp >= campaign.deadline(), "PreOrder: Deadline not reached");
-        require(pledgeCount >= minimumPledgeCount, "PreOrder: Campaign not successful");
-        uint256 balance = currentBalance();
-        uint256 protocolShare = balance * 200 / percentDivider;
-        uint256 platformShare = balance * 300 / percentDivider;
-        bool success = IERC20(token).transfer(platformAddress, platformShare);
-        require (success);
-        success = IERC20(token).transfer(protocolAddress, protocolShare);
-        require (success);
-        success = IERC20(token).transfer(campaign.creator(), currentBalance());
-        require (success);
-        
     }
 
     function claimRefund(uint256 tokenId) external {
@@ -117,30 +101,14 @@ contract PreOrder is ICampaignTreasury, ERC721Burnable {
         require(success);
     }
 
+    function _checkSuccessCondition() internal view override returns (bool) {
+        return (s_numberOfOrders > MINIMUM_NO_OF_ORDER_REQUIRED);
+    } 
+
     // The following functions are overrides required by Solidity.
     function supportsInterface(
         bytes4 interfaceId
     ) public view override returns (bool) {
         return super.supportsInterface(interfaceId);
-    }
-
-    function getplatformId() external view returns (bytes32) {
-        return platform;
-    }
-
-    function getplatformFeePercent() external view returns (uint256) {
-        return platformFeePercent;
-    }
-
-    function getplatformFee() external view returns (uint256) {
-        return (pledgedAmount * platformFeePercent) / percentDivider;
-    }
-
-    function setplatformFeePercent(uint256 _platformFeePercent) external {
-        platformFeePercent = _platformFeePercent;
-    }
-
-    function currentBalance() public view override returns (uint256) {
-        return IERC20(ICampaignInfo(info).token()).balanceOf(address(this));
     }
 }
