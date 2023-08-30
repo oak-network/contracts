a61cb003e4ce8ce318bef5ed9475e5697968619b
rayedsikder <rayedsikder33@gmail.com>
2023-08-30 12:51:33 +0600

    Add feature to enlist & delist bytecode

--- contracts/TreasuryFactory.sol
+++ contracts/TreasuryFactory.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.9;
 
 import "./CampaignInfo.sol";
-import "./models/AllOrNothing.sol";
-import "./models/KeepWhatsRaised.sol";
-import "./models/PreOrder.sol";
 import "./interfaces/IGlobalParams.sol";
 
 contract TreasuryFactory {
+    mapping(bytes32 => mapping(uint256 => bytes)) private s_platformByteCode;
+    mapping(bytes => bool) private s_approvedByteCode;
 
-    mapping (bytes32 => bytes) private platformByteCode;
     IGlobalParams private immutable GLOBAL_PARAMS;
 
     error TreasuryFactoryUnauthorized();
+    error TreasuryFactoryInvalidKey();
+    error TreasuryFactoryByteCodeAlreadyApproved();
 
     constructor(address globalParams) {
         GLOBAL_PARAMS = IGlobalParams(globalParams);
@@ -28,17 +28,49 @@ contract TreasuryFactory {
         _;
     }
 
-    function addByteCode(bytes32 platformBytes, bytes calldata byteCode) external onlyPlatformAdmin(platformBytes) {
-        platformByteCode[platformBytes] = byteCode;
+    function addByteCode(
+        bytes32 platformBytes,
+        uint256 bytecCodeIndex,
+        bytes calldata byteCode
+    ) external onlyPlatformAdmin(platformBytes) {
+        s_platformByteCode[platformBytes][bytecCodeIndex] = byteCode;
     }
 
-    function _checkIfPlatformAdmin(bytes32 platformBytes) private {
-        if (GLOBAL_PARAMS.getPlatformAdminAddress(platformBytes) != msg.sender) {
+    function enlistByteCode(
+        bytes32 platformBytes,
+        uint256 byteCodeIndex
+    ) external onlyProtocolAdmin {
+        bytes memory byteCode = s_platformByteCode[platformBytes][byteCodeIndex];
+        if (byteCode.length == 0) {
+            revert TreasuryFactoryInvalidKey();
+        }
+        if (s_approvedByteCode[byteCode]) {
+            revert TreasuryFactoryByteCodeAlreadyApproved();
+        }
+        s_approvedByteCode[byteCode] = true;
+    }
+
+    function delistByteCode(
+        bytes32 platformBytes,
+        uint256 byteCodeIndex
+    ) external onlyProtocolAdmin {
+        bytes storage byteCode = s_platformByteCode[platformBytes][byteCodeIndex];
+        if (byteCode.length == 0) {
+            revert TreasuryFactoryInvalidKey();
+        }
+        s_approvedByteCode[byteCode] = false;
+        delete s_platformByteCode[platformBytes][byteCodeIndex];
+    }
+
+    function _checkIfPlatformAdmin(bytes32 platformBytes) private view {
+        if (
+            GLOBAL_PARAMS.getPlatformAdminAddress(platformBytes) != msg.sender
+        ) {
             revert TreasuryFactoryUnauthorized();
         }
     }
 
-    function _checkIfProtocolAdmin() private {
+    function _checkIfProtocolAdmin() private view {
         if (GLOBAL_PARAMS.getProtocolAdminAddress() != msg.sender) {
             revert TreasuryFactoryUnauthorized();
         }
