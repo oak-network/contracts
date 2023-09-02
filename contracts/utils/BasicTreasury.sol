2b20f49e4db0114c9922f29a82cdb49831285e48
rayedsikder <rayedsikder33@gmail.com>
2023-09-02 17:03:53 +0600

    Add custom errors

--- contracts/utils/BasicTreasury.sol
+++ contracts/utils/BasicTreasury.sol
@@ -4,19 +4,28 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../interfaces/ICampaignInfo.sol";
 import "../interfaces/ICampaignTreasury.sol";
+import "../utils/AccessChecker.sol";
 
-abstract contract BasicTreasury is ICampaignTreasury {
+abstract contract BasicTreasury is ICampaignTreasury, AccessChecker {
     bytes32 internal constant ZERO_BYTES =
         0x0000000000000000000000000000000000000000000000000000000000000000;
     uint256 internal constant PERCENT_DIVIDER = 10000;
 
     bytes32 internal immutable PLATFORM_BYTES;
     uint256 internal immutable PLATFORM_FEE_PERCENT;
-    ICampaignInfo internal immutable INFO;
     IERC20 internal immutable TOKEN;
 
     uint256 internal s_pledgedAmountInCrypto;
     bool internal s_cryptoFeeDisbursed;
+
+    // Event emitted when fees are successfully disbursed
+    event FeesDisbursed(uint256 protocolShare, uint256 platformShare);
+
+    // Event emitted when a withdrawal is successful
+    event WithdrawalSuccessful(address to, uint256 amount);
+
+    // Event emitted when the success condition is not fulfilled during fee disbursement
+    event SuccessConditionNotFulfilled();
 
     error TreasuryTransferFailed();
     error TreasurySuccessConditionNotFulfilled();
@@ -27,48 +36,50 @@ abstract contract BasicTreasury is ICampaignTreasury {
         uint256 platformFeePercent,
         address infoAddress,
         address tokenAddress
-    ) {
+    ) AccessChecker(infoAddress) {
         PLATFORM_BYTES = platformBytes;
         PLATFORM_FEE_PERCENT = platformFeePercent;
-        INFO = ICampaignInfo(infoAddress);
         TOKEN = IERC20(tokenAddress);
     }
 
     function disburseFees() public virtual override {
-        uint256 balance = s_pledgedAmountInCrypto;
-        if (_checkSuccessCondition()) {
-            uint256 protocolShare = (balance * INFO.getProtocolFeePercent()) /
-                PERCENT_DIVIDER;
-            uint256 platformShare = (balance * INFO.getPlatformFeePercent(PLATFORM_BYTES)) /
-                PERCENT_DIVIDER;
-            bool success = TOKEN.transfer(
-                INFO.getProtocolAdminAddress(),
-                protocolShare
-            );
-            if (!success) {
-                revert TreasuryTransferFailed();
-            }
-            success = TOKEN.transfer(
-                INFO.getPlatformAdminAddress(PLATFORM_BYTES),
-                platformShare
-            );
-            if (!success) {
-                revert TreasuryTransferFailed();
-            }
-            s_cryptoFeeDisbursed = true;
-        } else {
+        if (!_checkSuccessCondition()) {
             revert TreasurySuccessConditionNotFulfilled();
         }
+        uint256 balance = s_pledgedAmountInCrypto;
+        uint256 protocolShare = (balance * INFO.getProtocolFeePercent()) /
+            PERCENT_DIVIDER;
+        uint256 platformShare = (balance *
+            INFO.getPlatformFeePercent(PLATFORM_BYTES)) / PERCENT_DIVIDER;
+        bool success = TOKEN.transfer(
+            INFO.getProtocolAdminAddress(),
+            protocolShare
+        );
+        if (!success) {
+            revert TreasuryTransferFailed();
+        }
+        success = TOKEN.transfer(
+            INFO.getPlatformAdminAddress(PLATFORM_BYTES),
+            platformShare
+        );
+        if (!success) {
+            revert TreasuryTransferFailed();
+        }
+        s_cryptoFeeDisbursed = true;
+        emit FeesDisbursed(protocolShare, platformShare);
     }
 
     function withdraw() public virtual override {
-        if (s_cryptoFeeDisbursed) {
-            uint256 balance = TOKEN.balanceOf(address(this));
-            bool success = TOKEN.transfer(INFO.owner(), balance);
-            if (!success) revert TreasuryTransferFailed();
-        } else {
+        if (!s_cryptoFeeDisbursed) {
             revert TreasuryFeeNotDisbursed();
         }
+        uint256 balance = TOKEN.balanceOf(address(this));
+        address recipient = INFO.owner();
+        bool success = TOKEN.transfer(recipient, balance);
+        if (!success) {
+            revert TreasuryTransferFailed();
+        }
+        emit WithdrawalSuccessful(recipient, balance);
     }
 
     function getplatformBytes() external view override returns (bytes32) {
