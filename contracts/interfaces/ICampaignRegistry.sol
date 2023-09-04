bac282a26b60d0bb2a54b30f78252a4c3a904183
rayedsikder <rayedsikder33@gmail.com>
2023-09-04 11:21:48 +0600

    Remove CampaignRegistry

--- contracts/interfaces/ICampaignRegistry.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
-
-interface ICampaignRegistry {
-    function getCampaignInfoFactoryAddress() external view returns (address);
-    function getTreasuryFactoryAddress() external view returns (address);
-    function getCampaignInfoAddress(bytes32 identifierHash) external view returns (address campaignAddress);
-    function setCampaignInfoAddress(bytes32 identifierHash, address campaignAddress) external;
-}
