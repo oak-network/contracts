ed4a3f08aedc2e6ab42238dc12f0e2c4cc34de87
Suvadra-Barua <suvadra@universalmachine.io>
2023-09-20 22:19:09 +0600

    Add foundry test file for openzeppelin Counters

---
+++ test/deps/Counters.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
+
+contract CountersTest is Test {}
