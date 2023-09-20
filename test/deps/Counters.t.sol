971903c42d5d6183655e7307685b503571e7bac1
Suvadra-Barua <suvadra@universalmachine.io>
2023-09-20 22:28:50 +0600

    Add function to test current value of OZ Counters

--- test/deps/Counters.t.sol
+++ test/deps/Counters.t.sol
@@ -4,4 +4,11 @@ pragma solidity ^0.8.19;
 import "forge-std/Test.sol";
 import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
 
-contract CountersTest is Test {}
+contract CountersTest is Test {
+    using Counters for Counters.Counter;
+    Counters.Counter _counter;
+
+    function test_Current() external {
+        assertEq(_counter.current(), 0);
+    }
+}
