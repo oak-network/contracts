f837e5619e509374f4ddef9d12c0d356e8e17c8b
rayedsikder <rayedsikder33@gmail.com>
2023-09-22 21:39:27 +0600

    Add function hashString in utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,10 @@
 import { ethers } from "hardhat";
 
-export const getHexString = (val: string) => {
+export const hexString = (val: string) => {
 const { hexlify, toUtf8Bytes, hexZeroPad } = ethers.utils;
     return hexZeroPad(hexlify(toUtf8Bytes(val)), 32);
 };
+
+export const hashString = (val: string) => {
+    return ethers.utils.keccak256(ethers.utils.toUtf8Bytes(val));
+}
