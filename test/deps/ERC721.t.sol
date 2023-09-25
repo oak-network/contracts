317ea7d8eafe6a477256ecf0647c5feccaa6f39e
Suvadra-Barua <suvadra@universalmachine.io>
2023-09-25 21:11:18 +0600

    Add function to test supportsInterface

--- test/deps/ERC721.t.sol
+++ test/deps/ERC721.t.sol
@@ -5,6 +5,9 @@ import "forge-std/Test.sol";
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {IERC721Metadata} from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+
 contract ERC721Mock is ERC721 {
     string private constant _NAME = "TestNFT";
     string private constant _SYMBOL = "TNFT";
@@ -24,5 +27,10 @@ contract ERC721Test is Test {
     function testInitialSetUP() external {
         assertEq(erc721.name(), _NAME);
         assertEq(erc721.symbol(), _SYMBOL);
+    }
+
+    function testSupportsInterfaceSuccess() public {
+        assertTrue(erc721.supportsInterface(type(IERC721).interfaceId));
+        assertTrue(erc721.supportsInterface(type(IERC721Metadata).interfaceId));
     }
 }
