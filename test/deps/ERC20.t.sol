9b67d5a80a20157d2053baaf9a0827875de04a6e
Suvadra-Barua <suvadra@universalmachine.io>
2023-09-25 18:33:07 +0600

    Add ERC20Mock and ERC20Test contract

---
+++ test/deps/ERC20.t.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20Mock is ERC20 {
+    string private constant _NAME = "TestToken";
+    string private constant _SYMBOL = "TT";
+    uint256 private constant _INITIAL_SUPPLY = type(uint8).max;
+
+    constructor() ERC20(_NAME, _SYMBOL) {}
+
+    function mint(address account, uint256 amount) external {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) external {
+        _burn(account, amount);
+    }
+}
+
+contract ERC20Test is Test {
+    string private constant _NAME = "TestToken";
+    string private constant _SYMBOL = "TT";
+    uint256 private constant _INITIAL_SUPPLY = type(uint8).max;
+    address deployer = address(0x12);
+
+    ERC20Mock erc20Contract;
+
+    function setUp() external {
+        vm.prank(deployer);
+        erc20Contract = new ERC20Mock();
+        erc20Contract.mint(deployer, _INITIAL_SUPPLY);
+    }
+}
