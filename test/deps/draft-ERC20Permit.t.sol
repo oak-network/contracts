cf5153771f7fd8c93d063cbd6d2527f4461f2a9d
Suvadra-Barua <suvadra@universalmachine.io>
2023-09-27 00:12:47 +0600

    Remove ERC712

--- test/deps/draft-ERC20Permit.t.sol
+++ test/deps/draft-ERC20Permit.t.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.19;
 import "forge-std/Test.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
-import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
 contract MockERC20Permit is ERC20Permit {
     string private constant _NAME = "TestToken";
