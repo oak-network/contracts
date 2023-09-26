9e56881b78f62f4321a572843496bbc0ceacd38a
Suvadra-Barua <suvadra@universalmachine.io>
2023-09-26 18:14:23 +0600

    Add function to test permit function

--- test/deps/draft-ERC20Permit.t.sol
+++ test/deps/draft-ERC20Permit.t.sol
@@ -13,3 +13,55 @@ contract MockERC20Permit is ERC20Permit {
 
     constructor() ERC20(_NAME, _SYMBOL) ERC20Permit(_NAME) {}
 }
+
+contract ERC20PermitTest is Test {
+    MockERC20Permit erc20Permit;
+    bytes32 private constant _PERMIT_TYPE_HASH =
+        keccak256(
+            bytes(
+                "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
+            )
+        );
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+
+    function setUp() external {
+        erc20Permit = new MockERC20Permit();
+    }
+
+    function testPermitSuccess() public {
+        (address owner, uint256 key) = makeAddrAndKey("owner");
+        address spender = makeAddr("spender");
+        uint256 amount = 100;
+        uint256 nonce = erc20Permit.nonces(owner);
+        uint256 deadline = block.timestamp + 100_000;
+        bytes32 domainSeparator = erc20Permit.DOMAIN_SEPARATOR();
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            key,
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    domainSeparator,
+                    keccak256(
+                        abi.encode(
+                            _PERMIT_TYPE_HASH,
+                            owner,
+                            spender,
+                            amount,
+                            nonce,
+                            deadline
+                        )
+                    )
+                )
+            )
+        );
+        vm.expectEmit(true, true, false, true);
+        emit Approval(owner, spender, amount);
+        erc20Permit.permit(owner, spender, amount, deadline, v, r, s);
+        assertEq(erc20Permit.allowance(owner, spender), amount);
+        assertEq(erc20Permit.nonces(owner), 1);
+    }
+}
