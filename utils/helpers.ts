687f7621d6e7d6e637004f901de99d7319c1b132
Mahabub Alahi <mahabub@universalmachine.io>
2023-09-27 12:34:45 +0600

    Add `splitByteCodeIntoChunks` `convertBigNumber`

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -10,3 +10,38 @@ export const convertBytesToString = (val: any) => {
     const { parseBytes32String } = ethers.utils;
     return parseBytes32String(val);
 };
+
+export const splitByteCodeIntoChunks = (byteCode: string, numberOfChunks: number) => {
+    const numChunks = Math.ceil(byteCode.length / numberOfChunks)
+    const chunks = new Array(numberOfChunks);
+    let o = 0;
+    for (let i = 0; i < numberOfChunks; i++) {
+        if( i !== 0 ) {
+            let chunk = byteCode.slice(o, numChunks + o);
+            if(chunk.length % 2 === 1){
+                chunks[i] = '0x' + byteCode.slice(o, (numChunks + o) - 1 )
+                o += numChunks - 1;
+            }else{
+                chunks[i] = '0x' + byteCode.slice(o, numChunks + o)
+                o += numChunks;
+            }
+            
+        }
+        else{
+            let chunk = byteCode.slice(o, numChunks);
+            if(chunk.length % 2 === 1){
+                chunks[i] = byteCode.slice(o, numChunks+1);
+                o += numChunks + 1;
+            }else{
+                chunks[i] = byteCode.slice(o, numChunks);
+                o += numChunks;
+            }
+        } 
+    }
+
+    return chunks;
+}
+
+export const convertBigNumber = (value: number, decimal: number) => {
+    return ethers.BigNumber.from(value).mul(ethers.BigNumber.from(10).pow(decimal));
+}
