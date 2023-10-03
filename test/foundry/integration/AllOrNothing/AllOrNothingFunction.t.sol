2a5ac2809ffa62e5839bacfa2c7870d2e1645fa1
Mahabub Alahi <mahabub@universalmachine.io>
2023-10-03 15:49:50 +0600

    Test: add AllOrNothing contract's integration test
    - Test functions for happy cases:-
       - test_addReward
       - test_pledgeOnPreLaunch
       - test_pledgeForAReward
       - test_pledgeWithoutAReward
       - test_claimRefund
       - test_disburseFees
       - test_withdraw

---
+++ test/foundry/integration/AllOrNothing/AllOrNothingFunction.t.sol
@@ -0,0 +1,135 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {AllOrNothing_Integration_Shared_Test} from "./AllOrNothing.t.sol";
+import "forge-std/Console.sol";
+import "forge-std/Vm.sol";
+import {AllOrNothing} from "src/treasuries/AllOrNothing.sol";
+
+contract AllOrNothingFunction_Integration_Shared_Test is AllOrNothing_Integration_Shared_Test {
+
+    function test_addReward() external {
+
+        addReward();
+        
+        AllOrNothing.Reward memory resultReward = allOrNothing.getReward(REWARD_NAME_1_BYTES);
+
+        assertEq(REWARD1.rewardValue, resultReward.rewardValue);
+        assertEq(REWARD1.isRewardTier, resultReward.isRewardTier);
+        assertEq(REWARD1.itemId[0], resultReward.itemId[0]);
+        assertEq(REWARD1.itemValue, resultReward.itemValue);
+        assertEq(REWARD1.itemQuantity, resultReward.itemQuantity);
+    }
+
+    function test_pledgeOnPreLaunch() external {
+
+        addReward();
+        pledgeOnPreLaunch();
+
+        uint256 backerBalance = testUSD.balanceOf(users.backer1Address);
+        uint256 treasuryBalance = testUSD.balanceOf(address(allOrNothing));
+        uint256 backerNftBalance = allOrNothing.balanceOf(users.backer1Address);
+        address nftOwnerAddress = allOrNothing.ownerOf(1);
+
+        assertEq(users.backer1Address, nftOwnerAddress);
+        assertEq(PRE_LAUNCH_PLEDGE_AMOUNT, TOKEN_MINT_AMOUNT-backerBalance);
+        assertEq(PRE_LAUNCH_PLEDGE_AMOUNT, treasuryBalance);
+        assertEq(1, backerNftBalance);
+    }
+
+    function test_pledgeForAReward() external {
+
+        addReward();
+        pledgeOnPreLaunch();
+        pledgeForAReward();
+
+        uint256 backerBalance = testUSD.balanceOf(users.backer1Address);
+        uint256 treasuryBalance = testUSD.balanceOf(address(allOrNothing));
+        uint256 backerNftBalance = allOrNothing.balanceOf(users.backer1Address);
+
+        address nftOwnerAddress = allOrNothing.ownerOf(2);
+
+        assertEq(users.backer1Address, nftOwnerAddress);
+        assertEq(PLEDGE_AMOUNT, TOKEN_MINT_AMOUNT - backerBalance - PRE_LAUNCH_PLEDGE_AMOUNT);
+        assertEq(PLEDGE_AMOUNT, treasuryBalance - PRE_LAUNCH_PLEDGE_AMOUNT);
+        assertEq(2, backerNftBalance);
+
+    }
+
+    function test_pledgeWithoutAReward() external {
+
+        addReward();
+        pledgeOnPreLaunch();
+        pledgeForAReward();
+        Vm.Log[] memory entries = pledgeWithoutAReward();
+
+        address resultBacker = address(uint160(uint(entries[3].topics[1])));
+
+        uint256 pledgeAmount;
+        uint256 tokenId;
+        bool isPreLaunchPledge;
+        bytes32[] memory rewards;
+
+        (pledgeAmount, tokenId, isPreLaunchPledge, rewards) = abi.decode(entries[3].data, (uint256,uint256,bool,bytes32[]));
+
+        assertEq(users.backer1Address, resultBacker);
+        assertEq(PLEDGE_AMOUNT, pledgeAmount);
+        assertEq(false, isPreLaunchPledge);
+    }
+
+    function test_claimRefund() external {
+
+        addReward();
+        pledgeOnPreLaunch();
+        pledgeForAReward();
+        pledgeWithoutAReward();
+        Vm.Log[] memory entries = claimRefund();
+
+        uint256 refundAmount;
+        uint256 tokenId;
+        address claimer;
+
+        (tokenId, refundAmount, claimer) = abi.decode(entries[4].data, (uint256,uint256,address));
+
+        assertEq(pledgeForARewardTokenId, tokenId);
+        assertEq(PLEDGE_AMOUNT, refundAmount);
+        assertEq(users.backer1Address, claimer);
+    }
+
+    function test_disburseFees() external {
+
+        addReward();
+        pledgeOnPreLaunch();
+        pledgeForAReward();
+        pledgeWithoutAReward();
+        claimRefund();
+        Vm.Log[] memory entries = disburseFees();
+
+        uint256 protocolShare;
+        uint256 platformShare;
+
+        (protocolShare, platformShare) = abi.decode(entries[2].data, (uint256,uint256));
+
+        assertEq(2_002e17, protocolShare);
+        assertEq(1_001e17, platformShare);
+    }
+
+    function test_withdraw() external {
+
+        addReward();
+        pledgeOnPreLaunch();
+        pledgeForAReward();
+        pledgeWithoutAReward();
+        claimRefund();
+        disburseFees();
+        Vm.Log[] memory entries = withdraw();
+
+        address to = address(uint160(uint(entries[1].topics[1])));
+        uint256 amount;
+
+        (amount) = abi.decode(entries[1].data, (uint256));
+
+        assertEq(users.creator1Address, to);
+        assertEq(7_007e17, amount);
+    }
+}
