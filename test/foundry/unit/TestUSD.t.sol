b9398756984e888dadb687dc9bd37810a1d2852a
Mahabub Alahi <mahabub@universalmachine.io>
2023-10-03 15:42:56 +0600

    Test: update `TestUSD_Test` contract
    - use contract owner for minting
    - add comments

--- test/foundry/unit/TestUSD.t.sol
+++ test/foundry/unit/TestUSD.t.sol
@@ -3,18 +3,22 @@ pragma solidity ^0.8.13;
 
 import {Base_Test} from "../Base.t.sol";
 
+/// @notice Test contract for TestUSD contract.
 contract TestUSD_Test is Base_Test {
     
     function setUp() public override {
        super.setUp();
+       vm.startPrank(users.contractOwner);
        testUSD.mint(users.creator1Address, 10e18);
+       vm.stopPrank();
     }
     
-
+    /// @dev Test mint function.
     function test_Mint() external {
         assertEq(testUSD.balanceOf(users.creator1Address), 10e18); 
     }
 
+    /// @dev Test TransferFrom function.
     function test_TransferFrom() external {
         vm.startPrank(users.creator1Address);
         testUSD.approve(address(this), 1e18);
