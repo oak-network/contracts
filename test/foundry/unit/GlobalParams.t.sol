b3f96da9b16ffe259bdb02462853f7b7cf2be583
Mahabub Alahi <mahabub@universalmachine.io>
2023-10-03 15:41:42 +0600

    Test: update `GlobalParams_Test` contract
    - add comment
    - update protocolFeePercent variable

--- test/foundry/unit/GlobalParams.t.sol
+++ test/foundry/unit/GlobalParams.t.sol
@@ -3,21 +3,25 @@ pragma solidity ^0.8.13;
 
 import {Base_Test} from "../Base.t.sol";
 
+/// @notice Test contract for GlobalParams contract.
 contract GlobalParams_Test is Base_Test {
 
+    /// @dev Test GetProtocolAdminAddress function.
     function test_GetProtocolAdminAddress() external {
         address returnProtocolAdminAddress = globalParams
             .getProtocolAdminAddress();
         assertEq(users.protocolAdminAddress, returnProtocolAdminAddress);
     }
 
+    /// @dev Test GetTokenAddress function.
     function test_GetTokenAddress() external {
         address tokenAddress = globalParams.getTokenAddress();
         assertEq(address(testUSD), tokenAddress);
     }
 
+    /// @dev Test GetProtocolFeePercent function.
     function test_GetProtocolFeePercent() external {
         uint256 returnProtocolFeePercent = globalParams.getProtocolFeePercent();
-        assertEq(protocolFeePercent, returnProtocolFeePercent);
+        assertEq(PROTOCOL_FEE_PERCENT, returnProtocolFeePercent);
     }
 }
