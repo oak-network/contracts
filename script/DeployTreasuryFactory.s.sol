f576381f76d0084f5363df109baa7922bc23bd34
rayedsikder <rayedsikder33@gmail.com>
2025-04-19 05:47:32 +0800

    Add deployment script for TreasuryFactory

---
+++ script/DeployTreasuryFactory.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {TreasuryFactory} from "../src/TreasuryFactory.sol";
+import {GlobalParams} from "../src/GlobalParams.sol";
+import {DeployBase} from "./lib/DeployBase.s.sol";
+
+contract DeployTreasuryFactory is DeployBase {
+    function deploy(address _globalParams) public returns (address) {
+        require(_globalParams != address(0), "GlobalParams not set");
+        return address(new TreasuryFactory(GlobalParams(_globalParams)));
+    }
+
+    function run() external {
+        address globalParams = vm.envOr("GLOBAL_PARAMS_ADDRESS", address(0));
+        require(globalParams != address(0), "GlobalParams must be set");
+
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+        deploy(globalParams);
+        vm.stopBroadcast();
+    }
+}
