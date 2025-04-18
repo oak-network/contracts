d5fec5912f95745464163d6fa3743d2370c03533
rayedsikder <rayedsikder33@gmail.com>
2025-04-19 05:28:39 +0800

    Configure ERC1167 proxy deployment for CampaignInfo

--- src/CampaignInfoFactory.sol
+++ src/CampaignInfoFactory.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
 import "./CampaignInfo.sol";
 import "./interfaces/IGlobalParams.sol";
 import "./interfaces/ICampaignInfoFactory.sol";
@@ -11,11 +13,12 @@ import "./interfaces/ICampaignInfoFactory.sol";
  * @notice Factory contract for creating campaign information contracts.
  */
 contract CampaignInfoFactory is ICampaignInfoFactory, Ownable {
-    bytes private constant bytecode = type(CampaignInfo).creationCode;
-
     IGlobalParams private GLOBAL_PARAMS;
     address private s_treasuryFactoryAddress;
     bool private s_initialized;
+    address private s_implementation;
+    mapping(address => bool) public isValidCampaignInfo;
+    mapping(bytes32 => address) public identifierToCampaignInfo;
 
     /**
      * @dev Emitted when the factory is initialized.
@@ -31,12 +34,18 @@ contract CampaignInfoFactory is ICampaignInfoFactory, Ownable {
      * @dev Emitted when campaign creation fails.
      */
     error CampaignInfoFactoryCampaignCreationFailed();
+    error CampaignInfoFactoryPlatformNotListed(bytes32 platformHash);
+    error CampaignInfoFactoryCampaignWithSameIdentifierExists(
+        bytes32 identifierHash,
+        address cloneExists
+    );
 
     /**
      * @param globalParams The address of the global parameters contract.
      */
-    constructor(IGlobalParams globalParams) {
+    constructor(IGlobalParams globalParams, address campaignImplementation) Ownable(msg.sender) {
         GLOBAL_PARAMS = globalParams;
+        s_implementation = campaignImplementation;
     }
 
     /**
@@ -56,13 +65,10 @@ contract CampaignInfoFactory is ICampaignInfoFactory, Ownable {
         s_initialized = true;
     }
 
-    /**
-     * @inheritdoc ICampaignInfoFactory
-     */
     function createCampaign(
         address creator,
         bytes32 identifierHash,
-        bytes32[] calldata selectedPlatformBytes,
+        bytes32[] calldata selectedPlatformHash,
         bytes32[] calldata platformDataKey,
         bytes32[] calldata platformDataValue,
         CampaignData calldata campaignData
@@ -70,23 +76,42 @@ contract CampaignInfoFactory is ICampaignInfoFactory, Ownable {
         if (platformDataKey.length != platformDataValue.length) {
             revert CampaignInfoFactoryInvalidInput();
         }
-        address info = address(
-            new CampaignInfo{salt: identifierHash}(
-                GLOBAL_PARAMS,
-                s_treasuryFactoryAddress,
-                GLOBAL_PARAMS.getTokenAddress(),
-                creator,
-                GLOBAL_PARAMS.getProtocolFeePercent(),
+        address cloneExists = identifierToCampaignInfo[identifierHash];
+        if (cloneExists != address(0)) {
+            revert CampaignInfoFactoryCampaignWithSameIdentifierExists(
                 identifierHash,
-                selectedPlatformBytes,
-                platformDataKey,
-                platformDataValue,
-                campaignData
-            )
-        );
-        if (info == address(0)) {
-            revert CampaignInfoFactoryCampaignCreationFailed();
+                cloneExists
+            );
         }
-        emit CampaignInfoFactoryCampaignCreated(identifierHash, info);
+        bool isListed;
+        bytes32 platformHash;
+        for (uint256 i = 0; i < selectedPlatformHash.length; i++) {
+            platformHash = selectedPlatformHash[i];
+            isListed = GLOBAL_PARAMS.checkIfPlatformIsListed(platformHash);
+            if (!isListed) {
+                revert CampaignInfoFactoryPlatformNotListed(platformHash);
+            }
+        }
+
+        bytes memory args = abi.encode(
+            s_treasuryFactoryAddress,
+            GLOBAL_PARAMS.getTokenAddress(),
+            GLOBAL_PARAMS.getProtocolFeePercent(),
+            identifierHash,
+            campaignData.launchTime,
+            campaignData.deadline,
+            campaignData.goalAmount
+        );
+        address clone = Clones.cloneWithImmutableArgs(s_implementation, args);
+        emit CampaignInfoFactoryCampaignCreated(identifierHash, clone);
+        CampaignInfo(clone).initialize(
+            creator,
+            GLOBAL_PARAMS,
+            selectedPlatformHash,
+            platformDataKey,
+            platformDataValue
+        );
+        identifierToCampaignInfo[identifierHash] = clone;
+        isValidCampaignInfo[clone] = true;
     }
 }
