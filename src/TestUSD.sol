420f3316ad974f44a2208c5da1bca263453017d0
rayedsikder <rayedsikder33@gmail.com>
2025-04-19 04:57:10 +0800

    Update mock token type

--- src/TestUSD.sol
+++ src/TestUSD.sol
@@ -1,16 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title TestUSD
  * @notice A test token `tUSD` which is used in the tests.
  */
-contract TestUSD is ERC20, ERC20Permit, Ownable {
-    constructor() ERC20("testUSD", "tUSD") ERC20Permit("testUSD") {}
+contract TestUSD is ERC20, Ownable {
+    constructor() ERC20("testUSD", "tUSD") Ownable(msg.sender) {}
 
     /**
      * @notice Mints testUSD token.
