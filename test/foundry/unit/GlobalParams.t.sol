b153c8c6025b765c4ab429d6b775982ef7a5c5fc
Adnan <adnan.haque1210@gmail.com>
2026-04-01 02:27:56 +0600

    add InvalidInput error codes for core contracts (#82)

--- test/foundry/unit/GlobalParams.t.sol
+++ test/foundry/unit/GlobalParams.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.22;
 
 import "forge-std/Test.sol";
 import {GlobalParams} from "src/GlobalParams.sol";
+import {ProtocolErrors} from "src/errors/ProtocolErrors.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {Defaults} from "../Base.t.sol";
 import {TestToken} from "../../mocks/TestToken.sol";
@@ -214,7 +215,12 @@ contract GlobalParams_UnitTest is Test, Defaults {
         vm.prank(admin);
         globalParams.enlistPlatform(platformHash, platformAdmin, 400, address(0));
 
-        vm.expectRevert(GlobalParams.GlobalParamsInvalidInput.selector);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                GlobalParams.GlobalParamsInvalidInput.selector,
+                ProtocolErrors.GlobalParamsInvalidInput.LINE_ITEM_GOAL_APPLIES_PROTOCOL_FEE
+            )
+        );
         vm.prank(platformAdmin);
         globalParams.setPlatformLineItemType(
             platformHash,
@@ -235,7 +241,12 @@ contract GlobalParams_UnitTest is Test, Defaults {
         vm.prank(admin);
         globalParams.enlistPlatform(platformHash, platformAdmin, 450, address(0));
 
-        vm.expectRevert(GlobalParams.GlobalParamsInvalidInput.selector);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                GlobalParams.GlobalParamsInvalidInput.selector,
+                ProtocolErrors.GlobalParamsInvalidInput.LINE_ITEM_GOAL_NOT_REFUNDABLE
+            )
+        );
         vm.prank(platformAdmin);
         globalParams.setPlatformLineItemType(
             platformHash,
@@ -256,7 +267,12 @@ contract GlobalParams_UnitTest is Test, Defaults {
         vm.prank(admin);
         globalParams.enlistPlatform(platformHash, platformAdmin, 300, address(0));
 
-        vm.expectRevert(GlobalParams.GlobalParamsInvalidInput.selector);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                GlobalParams.GlobalParamsInvalidInput.selector,
+                ProtocolErrors.GlobalParamsInvalidInput.LINE_ITEM_NON_GOAL_INSTANT_REFUNDABLE
+            )
+        );
         vm.prank(platformAdmin);
         globalParams.setPlatformLineItemType(
             platformHash,
@@ -313,7 +329,12 @@ contract GlobalParams_UnitTest is Test, Defaults {
             GlobalParams.initialize.selector, address(0), protocolFee, currencies, tokensPerCurrency
         );
 
-        vm.expectRevert(GlobalParams.GlobalParamsInvalidInput.selector);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                GlobalParams.GlobalParamsInvalidInput.selector,
+                ProtocolErrors.GlobalParamsInvalidInput.ZERO_ADDRESS
+            )
+        );
         new ERC1967Proxy(address(zeroAdminImpl), initData);
     }
 
